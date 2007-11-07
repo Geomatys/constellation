98b998c426feb23cc8356fb5d7cd8b638746892f
desruisseaux <devnull@localhost>
2007-11-07 16:11:10 +0000

    Added a set of shell scripts for convenience.

---
+++ postgrid/src/main/shell/etc/setenv.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# Définit les variables utilisées par tous les scripts du répertoire ../bin/.
+# Vous pouvez éditer ce fichier en fonction de la configuration de votre système.
+# Ces variables seront locales à l'exécution des scripts; elles n'affecteront pas
+# le système en dehors de ces scripts.
+#
+
+# Version des bibliothèques à utiliser par défaut
+export GEOTOOLS_VERSION=2.4-SNAPSHOT
+export SEAGIS_VERSION=1.0-SNAPSHOT
+
+# Répertoire contenant l'ensemble des fichiers JARS.
+export JARS="$BASE_DIR/jar"
+
+# Options communes à tous les programmes Java exécutés par les scripts.
+export OPTS="-Djava.util.logging.config.file=$BASE_DIR/etc/logging.properties"
+
