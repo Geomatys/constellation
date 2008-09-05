d74563dc2f267765c9a4d0256485885e967e9164
eclesia <devnull@localhost>
2008-09-05 14:09:39 +0000

    Added class header

--- web-services/services/src/main/java/net/seagis/query/WMSQuery.java
+++ web-services/services/src/main/java/net/seagis/query/WMSQuery.java
@@ -1,4 +1,17 @@
-
+/*
+ *    Sicade - Systèmes intégrés de connaissances pour l'aide à la décision en environnement
+ *    (C) 2008, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 2.1 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 package net.seagis.query;
 
 import java.awt.Color;
@@ -10,7 +23,7 @@ import org.geotools.sld.MutableStyledLayerDescriptor;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
- * WMS Query version 1.1.1
+ * WMS Query version
  * 
  * @author Johann Sorel (Geomatys)
  */
