7fd02f14d293c0542ae59e5605a07a683d235548
cedricbr <devnull@localhost>
2008-09-08 13:57:47 +0000

    security: move to LGPL v3 licence

--- security/xacmlSecurity/src/main/java/net/seagis/xacml/XACMLException.java
+++ security/xacmlSecurity/src/main/java/net/seagis/xacml/XACMLException.java
@@ -1,10 +1,13 @@
 /*
- * (C) 2007 Geomatys
+ *    Constellation - An open source and standard compliant SDI
+ *    http://constellation.codehaus.org
+ *
+ *    (C) 2007 - 2008, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    version 3 of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
--- security/xacmlSecurity/src/main/java/net/seagis/xacml/factory/PolicyAttributeFactory.java
+++ security/xacmlSecurity/src/main/java/net/seagis/xacml/factory/PolicyAttributeFactory.java
@@ -1,23 +1,19 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2007, JBoss Inc., and individual contributors as indicated
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ *    Constellation - An open source and standard compliant SDI
+ *    http://constellation.codehaus.org
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ *    (C) 2007, JBoss Inc.
+ *    (C) 2007 - 2008, Geomatys
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
  */
 package net.seagis.xacml.factory;
 
