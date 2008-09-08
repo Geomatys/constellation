02851854b2d7167d58aaaf9c172b060b469d5243
cedricbr <devnull@localhost>
2008-09-08 16:13:33 +0000

    widgets: move to LGPL v3 licence

--- widgets/src/main/java/net/seagis/gui/MarkColorChooser.java
+++ widgets/src/main/java/net/seagis/gui/MarkColorChooser.java
@@ -1,20 +1,19 @@
 /*
- * Sicade - Systèmes intégrés de connaissances pour l'aide à la décision en environnement
- * (C) 2005, Institut de Recherche pour le Développement
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2005, Institut de Recherche pour le Développement
+ *    (C) 2007 - 2008, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    version 3 of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package net.seagis.gui;
 
