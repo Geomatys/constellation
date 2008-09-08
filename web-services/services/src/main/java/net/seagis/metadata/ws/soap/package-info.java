e1a157099e1e7ec18436d27829087d27fa5580dc
cedricbr <devnull@localhost>
2008-09-08 16:48:50 +0000

    web-services/services: move to LGPL v3 licence

--- web-services/services/src/main/java/net/seagis/metadata/ws/soap/package-info.java
+++ web-services/services/src/main/java/net/seagis/metadata/ws/soap/package-info.java
@@ -1,3 +1,19 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2007 - 2008, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://sos.geomatys.fr/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
 xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = "g", namespaceURI= "http://sos.geomatys.fr/"),
           @javax.xml.bind.annotation.XmlNs(prefix = "ows", namespaceURI= "http://www.opengis.net/ows/1.1")})
