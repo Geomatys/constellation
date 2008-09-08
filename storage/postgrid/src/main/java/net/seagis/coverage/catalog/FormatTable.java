39e308c523662155f8a5da3e0391fe3dd5ac411d
cedricbr <devnull@localhost>
2008-09-08 16:08:50 +0000

    storage/postgrid: move to LGPL v3 licence

--- storage/postgrid/src/main/java/net/seagis/coverage/catalog/FormatTable.java
+++ storage/postgrid/src/main/java/net/seagis/coverage/catalog/FormatTable.java
@@ -1,12 +1,14 @@
 /*
- * Sicade - Systèmes intégrés de connaissances pour l'aide à la décision en environnement
- * (C) 2005, Institut de Recherche pour le Développement
- * (C) 2007, Geomatys
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2005, Institut de Recherche pour le Développement
+ *    (C) 2007 - 2008, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    version 3 of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
