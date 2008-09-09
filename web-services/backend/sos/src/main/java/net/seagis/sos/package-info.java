a0e720e2ea58f5b22df855147d4b793a57f0c9dc
cedricbr <devnull@localhost>
2008-09-09 09:28:04 +0000

    web-services/backend: move to LGPL v3 licence

--- web-services/backend/sos/src/main/java/net/seagis/sos/package-info.java
+++ web-services/backend/sos/src/main/java/net/seagis/sos/package-info.java
@@ -1,18 +1,19 @@
 /*
- * Sicade - Systemes integres de connaissances pour l'aide a la decision en environnement
- * (C) 2008 Geomatys
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2007 - 2008, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    version 3 of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
- 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/sos/1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
 xmlns = { @javax.xml.bind.annotation.XmlNs(prefix = "sos", namespaceURI= "http://www.opengis.net/sos/1.0"),
           @javax.xml.bind.annotation.XmlNs(prefix = "gml", namespaceURI= "http://www.opengis.net/gml"),
--- web-services/backend/wcs/src/main/java/net/seagis/wcs/v100/package-info.java
+++ web-services/backend/wcs/src/main/java/net/seagis/wcs/v100/package-info.java
@@ -1,17 +1,18 @@
 /*
- * Sicade - SystÃ¨mes intÃ©grÃ©s de connaissances pour l'aide Ã  la dÃ©cision en environnement
- * (C) 2005, Institut de Recherche pour le DÃ©veloppement
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2007 - 2008, Geomatys
  *
  *    This library is free software; you can redistribute it and/or
  *    modify it under the terms of the GNU Lesser General Public
  *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *    version 3 of the License, or (at your option) any later version.
  *
  *    This library is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Lesser General Public License for more details.
  */
-
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.opengis.net/wcs", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package net.seagis.wcs.v100;
