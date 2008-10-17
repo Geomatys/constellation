ded90f0da1d814d6eae082d324866090c6351b52
eclesia <devnull@localhost>
2008-10-17 15:22:16 +0000

    Test context builder for raster recolor

--- storage/postgrid/src/test/java/org/constellation/map/PostGRIDContextBuilder.java
+++ storage/postgrid/src/test/java/org/constellation/map/PostGRIDContextBuilder.java
@@ -8,7 +8,9 @@ import java.awt.Color;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javax.measure.unit.NonSI;
 import javax.measure.unit.Unit;
@@ -29,6 +31,7 @@ import org.geotools.style.StyleFactory;
 import org.geotools.style.function.InterpolationPoint;
 import org.geotools.style.function.Method;
 import org.geotools.style.function.Mode;
+import org.geotools.style.function.ThreshholdsBelongTo;
 import org.opengis.filter.expression.Expression;
 import org.opengis.filter.expression.Function;
 import org.opengis.filter.expression.Literal;
@@ -58,7 +61,7 @@ public class PostGRIDContextBuilder {
 
         try {
             context = new DefaultMapContext(DefaultGeographicCRS.WGS84);
-            context.layers().add(createPostGridLayer());
+            context.layers().add(createPostGridLayer2());
 
 //            context.setCoordinateReferenceSystem(layer.getFeatureSource().getSchema().getCoordinateReferenceSystem());
 //            context.setTitle("DemoContext");
@@ -81,13 +84,13 @@ public class PostGRIDContextBuilder {
         for (Layer lay : entries) {
             System.out.println(lay.getName());
         }
-        selectedLayer = layers.getEntry("SPOT5_Guyane_Panchro");
+//        selectedLayer = layers.getEntry("SPOT5_Guyane_Panchro");
 //        selectedLayer = layers.getEntry("BlueMarble");
-//        selectedLayer = layers.getEntry("AO_Coriolis_(Temp)");
+        selectedLayer = layers.getEntry("AO_Coriolis_(Temp)");
 //        selectedLayer = layers.getEntry("Mars3D_Gascogne_(UZ-VZ)");
 
         PostGridMapLayer layer = new PostGridMapLayer(database, selectedLayer);
-        layer.setStyle(createRasterStyle());
+        layer.setStyle(createInterpolationStyle());
 //        layer.graphicBuilders().add(new GridMarkGraphicBuilder());
 
         return layer;
@@ -105,28 +108,64 @@ public class PostGRIDContextBuilder {
         for (Layer lay : entries) {
             System.out.println(lay.getName());
         }
-        selectedLayer = layers.getEntry("SPOT5_Guyane_Panchro");
+//        selectedLayer = layers.getEntry("SPOT5_Guyane_Panchro");
 //        selectedLayer = layers.getEntry("BlueMarble");
-//        selectedLayer = layers.getEntry("AO_Coriolis_(Temp)");
+        selectedLayer = layers.getEntry("AO_Coriolis_(Temp)");
 //        selectedLayer = layers.getEntry("Mars3D_Gascogne_(UZ-VZ)");
 
         PostGridMapLayer2 layer = new PostGridMapLayer2(database, selectedLayer);
-        layer.setStyle(createRasterStyle());
+        layer.setStyle(createCategorizeStyle());
 //        layer.graphicBuilders().add(new GridMarkGraphicBuilder());
 
         return layer;
     }
 
-    public static MutableStyle createRasterStyle() {
+    public static MutableStyle createInterpolationStyle() {
 
         final List<InterpolationPoint> values = new ArrayList<InterpolationPoint>();
-        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.RED), 0));
-        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.GREEN), 10));
-        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.BLUE), 20));
+        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.WHITE), -10));
+        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.BLUE), 0));
+        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.YELLOW), 19));
+        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.GREEN), 20));
+        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.YELLOW), 21));
+        values.add(SF.createInterpolationPoint(SF.colorExpression(Color.RED), 30));
         final Literal lookup = StyleConstants.DEFAULT_CATEGORIZE_LOOKUP;
         final Literal fallback = StyleConstants.DEFAULT_FALLBACK;
-        final Function categorizeFunction = SF.createInterpolateFunction(
+        final Function interpolateFunction = SF.createInterpolateFunction(
                 lookup, values, Method.COLOR, Mode.LINEAR, fallback);
+
+        final ChannelSelection selection = SF.createChannelSelection(
+                SF.createSelectedChannelType("0", SF.literalExpression(1)));
+
+        Expression opacity = SF.literalExpression(1f);
+        OverlapBehavior overlap = OverlapBehavior.LATEST_ON_TOP;
+        ColorMap colorMap = SF.createColorMap(interpolateFunction);
+        ContrastEnhancement enchance = StyleConstants.DEFAULT_CONTRAST_ENHANCEMENT;
+        ShadedRelief relief = StyleConstants.DEFAULT_SHADED_RELIEF;
+        Symbolizer outline = null; //createRealWorldLineSymbolizer();
+        Unit uom = NonSI.FOOT;
+        String geom = StyleConstants.DEFAULT_GEOM;
+        String name = "raster symbol name";
+        Description desc = StyleConstants.DEFAULT_DESCRIPTION;
+
+        RasterSymbolizer symbol = SF.createRasterSymbolizer(opacity, selection, overlap, colorMap, enchance, relief, outline, uom, geom, name, desc);
+
+        return SF.createStyle(symbol);
+    }
+    
+    public static MutableStyle createCategorizeStyle() {
+
+        final Map<Expression,Expression> values = new HashMap<Expression,Expression>();
+        values.put(StyleConstants.CATEGORIZE_LESS_INFINITY, SF.colorExpression(Color.WHITE));
+        values.put(SF.literalExpression(0), SF.colorExpression(Color.BLUE));
+        values.put(SF.literalExpression(19), SF.colorExpression(Color.YELLOW));
+        values.put(SF.literalExpression(20), SF.colorExpression(Color.GREEN));
+        values.put(SF.literalExpression(21), SF.colorExpression(Color.YELLOW));
+        values.put(SF.literalExpression(30), SF.colorExpression(Color.RED));
+        final Literal lookup = StyleConstants.DEFAULT_CATEGORIZE_LOOKUP;
+        final Literal fallback = StyleConstants.DEFAULT_FALLBACK;
+        final Function categorizeFunction = SF.createCategorizeFunction(
+                lookup, values, ThreshholdsBelongTo.PRECEDING, fallback);
 
         final ChannelSelection selection = SF.createChannelSelection(
                 SF.createSelectedChannelType("0", SF.literalExpression(1)));
@@ -147,4 +186,6 @@ public class PostGRIDContextBuilder {
 
         return SF.createStyle(symbol);
     }
+    
+    
 }
