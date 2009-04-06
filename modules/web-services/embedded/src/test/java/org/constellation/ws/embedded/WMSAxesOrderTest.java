718a796d59c16fb4b2b162b29fbb7b56c167d826
cedricbr <devnull@localhost>
2009-04-06 10:05:48 +0000

    web-embedded: add a testing class for axes order in WMS GetMap requests.

---
+++ modules/web-services/embedded/src/test/java/org/constellation/ws/embedded/WMSAxesOrderTest.java
@@ -0,0 +1,270 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.constellation.ws.embedded;
+
+// J2SE dependencies
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+// Constellation dependencies
+import org.constellation.Cstl;
+import org.constellation.ServiceDef;
+import org.constellation.provider.LayerDetails;
+import org.constellation.register.RegisterException;
+import org.constellation.test.Commons;
+
+// JUnit dependencies
+import org.junit.*;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+
+/**
+ * Do some tests on the {@code WMS GetMap} request, in order to ensure that the axes
+ * order is well handled by Constellation, for different kinds of
+ * {@linkplain CoordinateReferenceSystem CRS}.
+ *
+ * @version $Id$
+ *
+ * @author Cédric Briançon (Geomatys)
+ * @since 0.3
+ */
+public class WMSAxesOrderTest extends AbstractGrizzlyServer {
+    /**
+     * A list of available layers to be requested in WMS.
+     */
+    private static List<LayerDetails> layers;
+
+    /**
+     * URLs which will be tested on the server.
+     */
+    private static final String WMS_GETMAP_111_PROJ =
+            "http://localhost:9090/wms?request=GetMap&service=WMS&version=1.1.1&" +
+                                      "format=image/png&width=1024&height=512&" +
+                                      "srs=EPSG:3395&bbox=-19000000,-19000000,19000000,19000000&" +
+                                      "layers="+ LAYER_TEST +"&styles=";
+    private static final String WMS_GETMAP_130_PROJ =
+            "http://localhost:9090/wms?request=GetMap&service=WMS&version=1.3.0&" +
+                                      "format=image/png&width=1024&height=512&" +
+                                      "crs=EPSG:3395&bbox=-19000000,-19000000,19000000,19000000&" +
+                                      "layers="+ LAYER_TEST +"&styles=";
+    private static final String WMS_GETMAP_111_GEO =
+            "http://localhost:9090/wms?request=GetMap&service=WMS&version=1.1.1&" +
+                                      "format=image/png&width=1024&height=512&" +
+                                      "srs=EPSG:4022&bbox=-90,-180,90,180&" +
+                                      "layers="+ LAYER_TEST +"&styles=";
+    private static final String WMS_GETMAP_111_WGS84 =
+            "http://localhost:9090/wms?request=GetMap&service=WMS&version=1.1.1&" +
+                                      "format=image/png&width=1024&height=512&" +
+                                      "srs=EPSG:4326&bbox=-180,-90,180,90&" +
+                                      "layers="+ LAYER_TEST +"&styles=";
+    private static final String WMS_GETMAP_130_WGS84 =
+            "http://localhost:9090/wms?request=GetMap&service=WMS&version=1.3.0&" +
+                                      "format=image/png&width=1024&height=512&" +
+                                      "crs=EPSG:4326&bbox=-90,-180,90,180&" +
+                                      "layers="+ LAYER_TEST +"&styles=";
+
+    /**
+     * Initialize the list of layers from the defined providers in Constellation's configuration.
+     */
+    @BeforeClass
+    public static void initLayerList() {
+        // Get the list of layers
+        try {
+            layers = Cstl.getRegister().getAllLayerReferences(ServiceDef.WMS_1_1_1_SLD);
+        } catch (RegisterException ex) {
+            layers = null;
+            assumeNoException(ex);
+        }
+    }
+
+    /**
+     * Free some resources.
+     */
+    @AfterClass
+    public static void finish() {
+        layers = null;
+    }
+
+    /**
+     * Ensures that a GetMap in version 1.1.1 on a projected
+     * {@linkplain CoordinateReferenceSystem CRS} provides the same image that a GetMap
+     * in version 1.3.0 on the same CRS.
+     */
+    @Test
+    public void testGetMap111And130Projected() {
+        assertNotNull(layers);
+        assumeTrue(!(layers.isEmpty()));
+        assumeTrue(containsTestLayer());
+
+        // Creates a valid GetMap url.
+        final URL getMap111Url, getMap130Url;
+        try {
+            getMap111Url = new URL(WMS_GETMAP_111_PROJ);
+            getMap130Url = new URL(WMS_GETMAP_130_PROJ);
+        } catch (MalformedURLException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Try to get a map from the url. The test is skipped in this method if it fails.
+        final BufferedImage image111, image130;
+        try {
+            image111 = getImageFromURL(getMap111Url, "image/png");
+            image130 = getImageFromURL(getMap130Url, "image/png");
+        } catch (IOException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Test on the returned image.
+        assertEquals(image111.getWidth(), 1024);
+        assertEquals(image111.getHeight(), 512);
+        assertEquals(Commons.checksum(image111), Commons.checksum(image130));
+        assertEquals(Commons.checksum(image130), 2274939253L);
+    }
+
+    /**
+     * Test a GetMap request on a WMS version 1.1.1 for a geographical
+     * {@linkplain CoordinateReferenceSystem CRS}.
+     *
+     * TODO: fix the implementation of the GetMap request concerning the handling of
+     *       geographical CRS (not WGS84) and do this test then.
+     */
+    @Test
+    @Ignore
+    public void testCRSGeographique111() {
+        assertNotNull(layers);
+        assumeTrue(!(layers.isEmpty()));
+        assumeTrue(containsTestLayer());
+
+        // Creates a valid GetMap url.
+        final URL getMapUrl;
+        try {
+            getMapUrl = new URL(WMS_GETMAP_111_GEO);
+        } catch (MalformedURLException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Try to get a map from the url. The test is skipped in this method if it fails.
+        final BufferedImage image;
+        try {
+            image = getImageFromURL(getMapUrl, "image/png");
+        } catch (IOException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Test on the returned image.
+        assertEquals(image.getWidth(), 1024);
+        assertEquals(image.getHeight(), 512);
+    }
+
+    /**
+     * Verify the axis order for a GetMap in version 1.1.1 for the {@code WGS84} CRS.
+     */
+    @Test
+    public void testGetMap111Wgs84() {
+        assertNotNull(layers);
+        assumeTrue(!(layers.isEmpty()));
+        assumeTrue(containsTestLayer());
+
+        // Creates a valid GetMap url.
+        final URL getMapUrl;
+        try {
+            getMapUrl = new URL(WMS_GETMAP_111_WGS84);
+        } catch (MalformedURLException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Try to get a map from the url. The test is skipped in this method if it fails.
+        final BufferedImage image;
+        try {
+            image = getImageFromURL(getMapUrl, "image/png");
+        } catch (IOException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Test on the returned image.
+        assertEquals(image.getWidth(), 1024);
+        assertEquals(image.getHeight(), 512);
+        assertEquals(Commons.checksum(image), 3640849032L);
+    }
+
+    /**
+     * Verify the axis order for a GetMap in version 1.3.0 for the {@code WGS84} CRS.
+     *
+     * TODO: fix the implementation of the GetMap request concerning the axes order,
+     *       and do this test then.
+     */
+    @Test
+    @Ignore
+    public void testGetMap130Wgs84() {
+        assertNotNull(layers);
+        assumeTrue(!(layers.isEmpty()));
+        assumeTrue(containsTestLayer());
+
+        // Creates a valid GetMap url.
+        final URL getMapUrl;
+        try {
+            getMapUrl = new URL(WMS_GETMAP_130_WGS84);
+        } catch (MalformedURLException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Try to get a map from the url. The test is skipped in this method if it fails.
+        final BufferedImage image;
+        try {
+            image = getImageFromURL(getMapUrl, "image/png");
+        } catch (IOException ex) {
+            assumeNoException(ex);
+            return;
+        }
+
+        // Test on the returned image.
+        assertEquals(image.getWidth(), 1024);
+        assertEquals(image.getHeight(), 512);
+        assertTrue  (!(Commons.isImageEmpty(image)));
+        // Here we have to ensure that the axis order should be lat,long in the GetMap request.
+        // So with that axes order, the image should be the same than the one done in GetMap
+        // version 1.1.1 with axes order long,lat.
+        assertEquals(Commons.checksum(image), 3640849032L);
+    }
+
+    /**
+     * Returns {@code true} if the {@code SST_tests} layer is found in the list of
+     * available layers. It means the postgrid database, pointed by the postgrid.xml
+     * file in the configuration directory, contains this layer and can then be requested
+     * in WMS.
+     */
+    private static boolean containsTestLayer() {
+        for (LayerDetails layer : layers) {
+            if (layer.getName().equals(LAYER_TEST)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
