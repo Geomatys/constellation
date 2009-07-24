3b79053970dcd8d129d4b38181e350be03337904
glegal <devnull@localhost>
2009-07-24 14:37:55 +0000

    add missing class

---
+++ modules/web-services/base/src/main/java/org/constellation/ws/MimeType.java
@@ -0,0 +1,56 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2005, Institut de Recherche pour le Développement
+ *    (C) 2007 - 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.constellation.ws;
+
+/**
+ *
+ * @author Guilhem Legal (Geomatys)
+ */
+public class MimeType {
+
+    private MimeType() {}
+
+    public static final String TEXT_XML        = "text/xml";
+
+    public static final String APPLICATION_XML = "application/xml";
+
+    public static final String TEXT_PLAIN      = "text/plain";
+
+    public static final String TEXT_HTML       = "text/html";
+
+    public static final String IMAGE_PNG       = "image/png";
+
+    public static final String IMAGE_GIF       = "image/gif";
+
+    public static final String IMAGE_JPEG      = "image/jpeg";
+
+    public static final String IMAGE_BMP       = "image/bmp";
+
+    public static final String APP_WMS_XML     = "application/vnd.ogc.wms_xml";
+
+    public static final String APP_INIMAGE     = "application/vnd.ogc.se_inimage";
+
+    public static final String APP_XML         = "application/vnd.ogc.xml";
+     
+    public static final String APP_GML         = "application/vnd.ogc.gml";
+
+    public static final String APP_SE_XML      = "application/vnd.ogc.se_xml";
+
+
+}
