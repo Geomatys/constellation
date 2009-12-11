2e9f177dc1ad50e47d2a7d1633ebc53e353cebe3
cedricbr <devnull@localhost>
2009-12-11 11:05:31 +0000

    CONSTELLATION-80: some unit test around WCSWorker

---
+++ modules/web-services/wcs/src/test/java/org/constellation/coverage/ws/WCSWorkerInit.java
@@ -0,0 +1,137 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.constellation.coverage.ws;
+
+import com.sun.grizzly.http.servlet.ServletContextImpl;
+import com.sun.jersey.core.header.InBoundHeaders;
+import com.sun.jersey.server.impl.application.WebApplicationContext;
+import com.sun.jersey.server.impl.application.WebApplicationImpl;
+import com.sun.jersey.spi.container.ContainerRequest;
+import java.net.URI;
+import java.util.List;
+import org.constellation.Cstl;
+import org.constellation.ServiceDef;
+import org.constellation.data.PostgridTestCase;
+import org.constellation.jaxb.AnchoredMarshallerPool;
+import org.constellation.provider.LayerDetails;
+import org.constellation.provider.LayerProviderProxy;
+import org.constellation.provider.LayerProviderService;
+import org.constellation.provider.configuration.ProviderConfig;
+import org.constellation.provider.configuration.ProviderSource;
+import org.constellation.provider.postgrid.PostGridProvider;
+import org.constellation.provider.postgrid.PostGridProviderService;
+import org.constellation.register.RegisterException;
+import org.geotoolkit.xml.MarshallerPool;
+import org.junit.BeforeClass;
+
+import static org.junit.Assume.*;
+
+
+/**
+ * Initializes a {@link WCSWorker} for testing GetCapabilities, DescribeCoverage and GetCoverage
+ * requests. Ensures that a PostGRID data preconfigured is handled by the {@link WCSWorker}.
+ *
+ * @version $Id$
+ * @author Cédric Briançon (Geomatys)
+ *
+ * @since 0.5
+ */
+public class WCSWorkerInit extends PostgridTestCase {
+    /**
+     * A list of available layers to be requested in WMS.
+     */
+    protected static List<LayerDetails> LAYERS;
+
+    /**
+     * The layer to test.
+     */
+    protected static final String LAYER_TEST = "SST_tests";
+
+    protected static WCSWorker WORKER;
+
+    protected static MarshallerPool POOL;
+
+    /**
+     * Initialisation of the worker and the PostGRID data provider before launching
+     * the different tests.
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        POOL = new AnchoredMarshallerPool("",
+                    "org.constellation.ws:" +
+                    "org.geotoolkit.ows.xml.v100:" +
+                    "org.geotoolkit.wcs.xml.v100:" +
+                    "org.geotoolkit.wcs.xml.v111:" +
+                    "org.geotoolkit.gml.xml.v311");
+        WORKER = new WCSWorker(POOL);
+        // Default instanciation of the worker' servlet context and uri context.
+        WORKER.internalInitServletContext(new ServletContextImpl());
+        WORKER.internalInitUriContext(new WebApplicationContext(
+                new WebApplicationImpl(),
+                new ContainerRequest(new WebApplicationImpl(), "GET", new URI("http://localhost:9090/"),
+                                     new URI("http://localhost:9090/wcs?request=GetCapabilities&service=WCS&version=1.0.0"),
+                                     new InBoundHeaders(), null),
+                null));
+
+        // Defines a PostGrid data provider
+        final ProviderSource source = new ProviderSource();
+        source.parameters.put(PostGridProvider.KEY_DATABASE, "jdbc:postgresql://hyperion.geomatys.com/coverages-test");
+        source.parameters.put(PostGridProvider.KEY_DRIVER,   "org.postgresql.Driver");
+        source.parameters.put(PostGridProvider.KEY_PASSWORD, "g3ouser");
+        source.parameters.put(PostGridProvider.KEY_READONLY, "true");
+        final String rootDir = System.getProperty("java.io.tmpdir") + "/Constellation/images";
+        source.parameters.put(PostGridProvider.KEY_ROOT_DIRECTORY, rootDir);
+        source.parameters.put(PostGridProvider.KEY_USER,     "geouser");
+
+        final ProviderConfig config = new ProviderConfig();
+        config.sources.add(source);
+
+        for (LayerProviderService service : LayerProviderProxy.getInstance().getServices()) {
+            // Here we should have the postgrid data provider defined previously
+            if (service instanceof PostGridProviderService) {
+                service.setConfiguration(config);
+                assumeTrue(!(service.getProviders().isEmpty()));
+                if (service.getProviders().isEmpty()) {
+                    return;
+                }
+                break;
+            }
+        }
+
+        try {
+            LAYERS = Cstl.getRegister().getAllLayerReferences(ServiceDef.WCS_1_0_0);
+        } catch (RegisterException ex) {
+            LAYERS = null;
+            assumeNoException(ex);
+        }
+    }
+
+    /**
+     * Returns {@code true} if the {@code SST_tests} layer is found in the list of
+     * available layers. It means the postgrid database, pointed by the postgrid.xml
+     * file in the configuration directory, contains this layer and can then be requested
+     * in WMS.
+     */
+    protected static boolean containsTestLayer() {
+        for (LayerDetails layer : LAYERS) {
+            if (layer.getName().equals(LAYER_TEST)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ modules/web-services/wcs/src/test/java/org/constellation/coverage/ws/WCSWorkerOutputTest.java
@@ -0,0 +1,141 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2009, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.constellation.coverage.ws;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import org.constellation.ws.CstlServiceException;
+import org.geotoolkit.gml.xml.v311.DirectPositionType;
+import org.geotoolkit.gml.xml.v311.EnvelopeEntry;
+import org.geotoolkit.gml.xml.v311.TimePositionType;
+import org.geotoolkit.wcs.xml.DescribeCoverage;
+import org.geotoolkit.wcs.xml.DescribeCoverageResponse;
+import org.geotoolkit.wcs.xml.GetCapabilities;
+import org.geotoolkit.wcs.xml.GetCapabilitiesResponse;
+import org.geotoolkit.wcs.xml.v100.CoverageDescription;
+import org.geotoolkit.wcs.xml.v100.CoverageOfferingBriefType;
+import org.geotoolkit.wcs.xml.v100.CoverageOfferingType;
+import org.geotoolkit.wcs.xml.v100.DescribeCoverageType;
+import org.geotoolkit.wcs.xml.v100.GetCapabilitiesType;
+import org.geotoolkit.wcs.xml.v100.SpatialDomainType;
+import org.geotoolkit.wcs.xml.v100.TimeSequenceType;
+import org.geotoolkit.wcs.xml.v100.WCSCapabilitiesType;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.*;
+
+
+/**
+ * Testing class for WCS requests.
+ *
+ * @version $Id$
+ * @author Cédric Briançon (Geomatys)
+ *
+ * @since 0.5
+ */
+public class WCSWorkerOutputTest extends WCSWorkerInit {
+    /**
+     * Ensures that a PostGRID layer preconfigured is found in the GetCapabilities document
+     * returned by the {@link WCSWorker}.
+     *
+     * @throws JAXBException
+     * @throws CstlServiceException
+     */
+    @Test
+    public void testGetCapabilities() throws JAXBException, CstlServiceException {
+        final GetCapabilities request = new GetCapabilitiesType(null, null);
+        final GetCapabilitiesResponse response = WORKER.getCapabilities(request);
+
+        assertNotNull(response);
+        assertTrue(response instanceof WCSCapabilitiesType);
+        final WCSCapabilitiesType getCaps = (WCSCapabilitiesType) response;
+
+        // Ensures that the test layer is really present.
+        assumeTrue(containsTestLayer());
+        // Verifies that the test layer is present into the GetCapabilities response.
+        final List<CoverageOfferingBriefType> offerings = getCaps.getContentMetadata().getCoverageOfferingBrief();
+        assertFalse(offerings.isEmpty());
+        for (CoverageOfferingBriefType offering : offerings) {
+            for (JAXBElement<String> string : offering.getRest()) {
+                if (string.getName().getLocalPart().equalsIgnoreCase("name") &&
+                    string.getValue().equals(LAYER_TEST))
+                {
+                    return;
+                }
+            }
+        }
+        // Not found in the list of coverage offerings, there is a mistake here.
+        fail("Unable to find the layer "+ LAYER_TEST +" in the GetCapabilities document.");
+    }
+
+    /**
+     * Ensures that a PostGRID layer preconfigured can be requested with a DescribeCoverage request,
+     * and that the output document contains all data information.
+     *
+     * @throws JAXBException
+     * @throws CstlServiceException
+     */
+    @Test
+    public void testDescribeCoverage() throws JAXBException, CstlServiceException {
+        // Ensures that the test layer is really present.
+        assumeTrue(containsTestLayer());
+
+        final DescribeCoverage request = new DescribeCoverageType(LAYER_TEST);
+        final DescribeCoverageResponse response = WORKER.describeCoverage(request);
+        assertNotNull(response);
+        assertTrue(response instanceof CoverageDescription);
+
+//        final Marshaller marshaller = POOL.acquireMarshaller();
+//        marshaller.marshal(response, System.out);
+//        POOL.release(marshaller);
+
+        final CoverageDescription descCov = (CoverageDescription) response;
+        // Verifies that the test layer is present into the DescribeCoverage response.
+        for (CoverageOfferingType offering : descCov.getCoverageOffering()) {
+            for (JAXBElement<String> string : offering.getRest()) {
+                if (string.getName().getLocalPart().equalsIgnoreCase("name") &&
+                    string.getValue().equals(LAYER_TEST))
+                {
+                    final SpatialDomainType spatialDomain = (SpatialDomainType) offering.getDomainSet()
+                            .getContent().get(0).getValue();
+                    final TimeSequenceType temporalDomain = (TimeSequenceType) offering.getDomainSet()
+                            .getContent().get(1).getValue();
+                    // Builds expected spatial domain
+                    final List<DirectPositionType> pos = new ArrayList<DirectPositionType>();
+                    pos.add(new DirectPositionType(-180.0, -90.0));
+                    pos.add(new DirectPositionType(180.0, 90.0));
+                    final EnvelopeEntry expectedEnvelope = new EnvelopeEntry(pos, "urn:ogc:def:crs:OGC:1.3:CRS84");
+                    // Builds expected temporal domain
+                    final List<TimePositionType> expectedTimes =
+                            Collections.singletonList(new TimePositionType("2003-05-16T00:00:00Z"));
+                    // Do assertions
+                    assertEquals(expectedEnvelope, spatialDomain.getEnvelope());
+                    assertEquals(expectedTimes, temporalDomain.getTimePositionOrTimePeriod());
+                    /*
+                     * All tests have succeed on that specific layer, we can now stop this test.
+                     */
+                    return;
+                }
+            }
+        }
+        fail("Unable to find the layer "+ LAYER_TEST +" in the DescribeCoverage document.");
+    }
+}
