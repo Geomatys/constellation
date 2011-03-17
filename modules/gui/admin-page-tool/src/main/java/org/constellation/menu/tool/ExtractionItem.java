9c04a4afc07095f280c123602da3c9b142a27ef8
Johann Sorel <johann.sorel@geomatys.fr>
2011-03-17 18:19:48 +0100

    GUI : add tools page

---
+++ modules/gui/admin-page-tool/src/main/java/org/constellation/menu/tool/ExtractionItem.java
@@ -0,0 +1,37 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2011, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.constellation.menu.tool;
+
+import org.constellation.bean.AbstractMenuItem;
+
+
+/**
+ * Add a automated extraction tool page.
+ *
+ * @author Johann Sorel (Geomatys)
+ */
+public class ExtractionItem extends AbstractMenuItem{
+
+
+    public ExtractionItem() {
+        super(null,
+            new Path(TOOLS_PATH,"extraction", "/org/constellation/menu/tool/extraction.xhtml", "org.constellation.icons.antenna.png.mfRes",100)
+            );
+    }
+
+}
