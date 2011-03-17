2143acc001f339579b264f4f063b1e00e4b3002b
Johann Sorel <johann.sorel@geomatys.fr>
2011-03-17 17:13:47 +0100

    GUI : add empty administration page for all services

---
+++ modules/web-services/csw/src/main/java/org/constellation/menu/service/CSWItem.java
@@ -0,0 +1,36 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2011, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.constellation.menu.service;
+
+import org.constellation.bean.AbstractMenuItem;
+
+
+/**
+ * Add a CSW service page.
+ *
+ * @author Johann Sorel (Geomatys)
+ */
+public class CSWItem extends AbstractMenuItem{
+
+    public CSWItem() {
+        super("org.constellation.menu.service.csw",
+            new Path(SERVICES_PATH,"CSW", "/org/constellation/menu/service/csw.xhtml", null,600)
+            );
+    }
+
+}
