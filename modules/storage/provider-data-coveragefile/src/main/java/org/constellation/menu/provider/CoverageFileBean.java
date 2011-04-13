b33d2a9261aa50c18d8415fc91b5e511ef1e2806
Johann Sorel <johann.sorel@geomatys.fr>
2011-04-13 15:07:51 +0200

    Coverage-File & Coverage-Mosaic : add administration configuration page

---
+++ modules/storage/provider-data-coveragefile/src/main/java/org/constellation/menu/provider/CoverageFileBean.java
@@ -0,0 +1,51 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2011, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+
+package org.constellation.menu.provider;
+
+import org.constellation.provider.LayerProviderProxy;
+import org.constellation.provider.LayerProviderService;
+import org.constellation.provider.coveragefile.CoverageFileProvider;
+
+/**
+ * Coverage-File configuration bean.
+ *
+ * @author Johann Sorel (Geomatys)
+ */
+public class CoverageFileBean extends AbstractDataStoreServiceBean{
+
+    private static LayerProviderService getService(){
+        for(LayerProviderService service : LayerProviderProxy.getInstance().getServices()){
+            if(service.getName().equals("coverage-file")){
+                return service;
+            }
+        }
+        return null;
+    }
+
+    public CoverageFileBean(){
+        super(getService(),"/org/constellation/menu/provider/coveragefile.xhtml",
+              "/org/constellation/menu/provider/coveragefileConfig.xhtml");
+        addBundle("org.constellation.menu.provider.coveragefile");
+    }
+
+    @Override
+    protected Class getProviderClass() {
+        return CoverageFileProvider.class;
+    }
+
+}
