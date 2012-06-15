fc9da95a0e4338d78fccccd2bfd27d72729b50aa
Johann Sorel <johann.sorel@geomatys.fr>
2012-06-15 10:49:41 +0200

    Coverage-store : implement getbounds and getcoverage methods for coverage-store instances

--- modules/storage/provider/src/main/java/org/constellation/provider/DefaultCoverageStoreLayerDetails.java
+++ modules/storage/provider/src/main/java/org/constellation/provider/DefaultCoverageStoreLayerDetails.java
@@ -19,12 +19,18 @@ package org.constellation.provider;
 import java.awt.Dimension;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.CancellationException;
+import java.util.logging.Level;
 import org.constellation.ServiceDef.Query;
 import org.geotoolkit.coverage.CoverageReference;
+import org.geotoolkit.coverage.grid.GeneralGridGeometry;
 import org.geotoolkit.coverage.grid.GridCoverage2D;
+import org.geotoolkit.coverage.io.GridCoverageReadParam;
+import org.geotoolkit.coverage.io.GridCoverageReader;
 import org.geotoolkit.display.exception.PortrayalException;
 import org.geotoolkit.map.MapBuilder;
 import org.geotoolkit.map.MapLayer;
+import org.geotoolkit.metadata.iso.extent.DefaultGeographicBoundingBox;
 import org.geotoolkit.storage.DataStoreException;
 import org.geotoolkit.style.DefaultStyleFactory;
 import org.geotoolkit.style.MutableStyle;
@@ -33,6 +39,7 @@ import org.geotoolkit.util.MeasurementRange;
 import org.opengis.feature.type.Name;
 import org.opengis.geometry.Envelope;
 import org.opengis.metadata.extent.GeographicBoundingBox;
+import org.opengis.referencing.operation.TransformException;
 
 
 /**
@@ -59,12 +66,41 @@ public class DefaultCoverageStoreLayerDetails extends AbstractLayerDetails {
     
     @Override
     public GridCoverage2D getCoverage(Envelope envelope, Dimension dimension, Double elevation, Date time) throws DataStoreException, IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        final GridCoverageReader reader = ref.createReader();
+        
+        final GridCoverageReadParam param = new GridCoverageReadParam();
+        param.setEnvelope(envelope);
+        try {
+            return (GridCoverage2D) reader.read(0, param);
+        } catch (CancellationException ex) {
+            throw new IOException(ex.getMessage(),ex);
+        }finally{
+            reader.dispose();
+        }
+        
     }
 
     @Override
     public GeographicBoundingBox getGeographicBoundingBox() throws DataStoreException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        final GridCoverageReader reader = ref.createReader();
+        
+        try {
+            final GeneralGridGeometry generalGridGeom = reader.getGridGeometry(0);
+            if (generalGridGeom == null) {
+                LOGGER.log(Level.INFO, "The layer \"{0}\" does not contain a grid geometry information.", name);
+                return null;
+            }
+            
+            final Envelope env = generalGridGeom.getEnvelope();
+            return new DefaultGeographicBoundingBox(env);
+        } catch (CancellationException ex) {
+            throw new DataStoreException(ex);
+        } catch (TransformException ex) {
+            throw new DataStoreException(ex);
+        } finally {
+            reader.dispose();
+        }
+        
     }
 
     @Override
