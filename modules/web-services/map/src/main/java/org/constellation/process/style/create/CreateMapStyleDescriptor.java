6d5491687e85233ec6e55031cf307bfa77d41670
Quentin Boileau <quentin.boileau@geomatys.com>
2012-06-26 17:39:57 +0200

    PEAREP-183 : add createMapStyle process.

---
+++ modules/web-services/map/src/main/java/org/constellation/process/style/create/CreateMapStyleDescriptor.java
@@ -0,0 +1,86 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2012, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.constellation.process.style.create;
+
+import org.constellation.process.ConstellationProcessFactory;
+import org.geotoolkit.parameter.DefaultParameterDescriptor;
+import org.geotoolkit.parameter.DefaultParameterDescriptorGroup;
+import org.geotoolkit.process.AbstractProcessDescriptor;
+import org.geotoolkit.style.MutableStyle;
+import org.geotoolkit.util.SimpleInternationalString;
+import org.opengis.parameter.GeneralParameterDescriptor;
+import org.opengis.parameter.ParameterDescriptor;
+import org.opengis.parameter.ParameterDescriptorGroup;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.util.InternationalString;
+
+/**
+ * Add a style to an existing style provider.
+ *
+ * @author Quentin Boileau (Geomatys).
+ */
+public class CreateMapStyleDescriptor extends AbstractProcessDescriptor {
+
+    public static final String NAME = "createMapStyle";
+    public static final InternationalString ABSTRACT = new SimpleInternationalString("Attach a new style to a specified provider.");
+
+    /*
+     * Provider identifier
+     */
+    public static final String PROVIDER_ID_NAME = "provider_id";
+    private static final String PROVIDER_ID_REMARKS = "Identifier of the provider to set style.";
+    public static final ParameterDescriptor<String> PROVIDER_ID =
+            new DefaultParameterDescriptor(PROVIDER_ID_NAME, PROVIDER_ID_REMARKS, String.class, null, true);
+
+    /*
+     * Style name.
+     */
+    public static final String STYLE_ID_NAME = "style_Name";
+    private static final String STYLE_ID_REMARKS = "Name/Identifier of the style.";
+    public static final ParameterDescriptor<String> STYLE_ID =
+            new DefaultParameterDescriptor(STYLE_ID_NAME, STYLE_ID_REMARKS, String.class, null, true);
+
+    /*
+     * Style
+     */
+    public static final String STYLE_NAME = "style";
+    private static final String STYLE_REMARKS = "Style to add.";
+    public static final ParameterDescriptor<MutableStyle> STYLE =
+            new DefaultParameterDescriptor(STYLE_NAME, STYLE_REMARKS, MutableStyle.class, null, true);
+
+    /**
+     * Input parameters
+     */
+    public static final ParameterDescriptorGroup INPUT_DESC =
+            new DefaultParameterDescriptorGroup("InputParameters", new GeneralParameterDescriptor[]{PROVIDER_ID, STYLE_ID, STYLE});
+    /**
+     * Output parameters
+     */
+    public static final ParameterDescriptorGroup OUTPUT_DESC = new DefaultParameterDescriptorGroup("OutputParameters");
+
+    /**
+     * Public constructor use by the ServiceRegistry to find and intanciate all ProcessDescriptor.
+     */
+    public CreateMapStyleDescriptor() {
+        super(NAME, ConstellationProcessFactory.IDENTIFICATION, ABSTRACT, INPUT_DESC, OUTPUT_DESC);
+    }
+
+    @Override
+    public org.geotoolkit.process.Process createProcess(ParameterValueGroup input) {
+        return new CreateMapStyle(this, input);
+    }
+}
---
+++ modules/web-services/map/src/test/java/org/constellation/process/style/AbstractMapStyleTest.java
@@ -0,0 +1,132 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ *    (C) 2012, Geomatys
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation; either
+ *    version 3 of the License, or (at your option) any later version.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.constellation.process.style;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import org.constellation.configuration.ConfigDirectory;
+import org.constellation.process.AbstractProcessTest;
+import org.constellation.provider.*;
+import org.geotoolkit.util.FileUtilities;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.opengis.parameter.ParameterDescriptorGroup;
+import org.opengis.parameter.ParameterValueGroup;
+
+/**
+ *
+ * @author Quentin Boileau (Geomatys).
+ */
+public abstract class AbstractMapStyleTest extends AbstractProcessTest {
+
+    protected static File configDirectory;
+    protected static URL EMPTY_CSV;
+    // dataStore service
+    protected static ProviderService SLD_SERVICE;
+
+    static {
+        final Collection<StyleProviderService> availableLayerServices = StyleProviderProxy.getInstance().getServices();
+        for (StyleProviderService tmpService : availableLayerServices) {
+            if ("sld".equals(tmpService.getName())) {
+                SLD_SERVICE = tmpService;
+            }
+        }
+    }
+
+    public AbstractMapStyleTest(final String str) {
+        super(str);
+    }
+
+    @BeforeClass
+    public static void initFolder() throws MalformedURLException {
+
+        configDirectory = new File("ProcessProviderTest");
+
+        if (configDirectory.exists()) {
+            FileUtilities.deleteDirectory(configDirectory);
+        }
+
+        configDirectory.mkdir();
+        File providerDirectory = new File(configDirectory, "provider");
+        providerDirectory.mkdir();
+        File sld = new File(providerDirectory, "sld.xml");
+        ConfigDirectory.setConfigDirectory(configDirectory);
+
+        File csv = new File(configDirectory, "file.csv");
+        EMPTY_CSV = csv.toURI().toURL();
+
+    }
+
+    @AfterClass
+    public static void destroyFolder() {
+        FileUtilities.deleteDirectory(configDirectory);
+    }
+
+    /**
+     *
+     *
+     * @param sercice
+     * @param providerID
+     * @return
+     * @throws MalformedURLException
+     */
+    protected static ParameterValueGroup buildProvider(final String providerID, final boolean loadAll) throws MalformedURLException {
+
+        ParameterDescriptorGroup desc = SLD_SERVICE.getServiceDescriptor();
+
+        if (desc != null) {
+            final ParameterDescriptorGroup sourceDesc = (ParameterDescriptorGroup) desc.descriptor("source");
+            final ParameterValueGroup sourceValue = sourceDesc.createValue();
+            sourceValue.parameter("id").setValue(providerID);
+            sourceValue.parameter("load_all").setValue(loadAll);
+
+            final ParameterValueGroup sldFolderValue = sourceValue.groups("sldFolder").get(0);
+            final File sldFolder = new File(configDirectory, "sldDir");
+            sldFolder.mkdir();
+            sldFolderValue.parameter("path").setValue(sldFolder.getAbsolutePath());
+            return sourceValue;
+        } else {
+            //error
+            return null;
+        }
+    }
+
+    /**
+     * Register a provider.
+     * @param providerSource
+     */
+    protected static void addProvider(ParameterValueGroup providerSource) {
+        StyleProviderProxy.getInstance().createProvider((StyleProviderService) SLD_SERVICE, providerSource);
+    }
+
+    /**
+     * Un-register a provider
+     * @param id
+     */
+    protected static void removeProvider(String id) {
+
+        StyleProvider provider = null;
+        for (StyleProvider p : StyleProviderProxy.getInstance().getProviders()) {
+            if (p.getId().equals(id)) {
+            }
+        }
+        StyleProviderProxy.getInstance().removeProvider(provider);
+    }
+
+}
