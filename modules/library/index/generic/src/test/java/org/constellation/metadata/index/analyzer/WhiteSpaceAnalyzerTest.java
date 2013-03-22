b1689234398a51bd6d1c8f15a5d1f00951f34759
Guilhem Legal <guilhem.legal@geomatys.fr>
2013-03-22 17:50:27 +0100

    Index: update lucene version in test

--- modules/library/index/generic/src/test/java/org/constellation/metadata/index/analyzer/WhiteSpaceAnalyzerTest.java
+++ modules/library/index/generic/src/test/java/org/constellation/metadata/index/analyzer/WhiteSpaceAnalyzerTest.java
@@ -60,10 +60,10 @@ public class WhiteSpaceAnalyzerTest extends AbstractAnalyzerTest {
     public static void setUpClass() throws Exception {
         FileUtilities.deleteDirectory(configDirectory);
         List<Object> object = fillTestData();
-        GenericIndexer indexer = new GenericIndexer(object, null, configDirectory, "", new WhitespaceAnalyzer(Version.LUCENE_36), Level.FINER);
+        GenericIndexer indexer = new GenericIndexer(object, null, configDirectory, "", new WhitespaceAnalyzer(Version.LUCENE_40), Level.FINER);
         indexer.destroy();
 
-        indexSearcher          = new LuceneIndexSearcher(configDirectory, "", new WhitespaceAnalyzer(Version.LUCENE_36), true);
+        indexSearcher          = new LuceneIndexSearcher(configDirectory, "", new WhitespaceAnalyzer(Version.LUCENE_40), true);
         indexSearcher.setLogLevel(Level.FINER);
     }
 
