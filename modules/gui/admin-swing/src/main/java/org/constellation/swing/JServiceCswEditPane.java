9aca3cc8c8fad126c5a2faf47267b1cef4a04c58
Guilhem Legal <guilhem.legal@geomatys.fr>
2013-07-23 16:10:27 +0200

    Admin swing: add log Level configuration in CSW pane

--- modules/gui/admin-swing/src/main/java/org/constellation/swing/JServiceCswEditPane.java
+++ modules/gui/admin-swing/src/main/java/org/constellation/swing/JServiceCswEditPane.java
@@ -36,7 +36,6 @@ import org.constellation.admin.service.ConstellationServer;
 import org.constellation.configuration.Instance;
 import org.constellation.generic.database.Automatic;
 import static org.constellation.swing.JServiceEditionPane.LOGGER;
-import org.openide.util.NbBundle;
 
 /**
  *
@@ -76,6 +75,9 @@ public class JServiceCswEditPane extends JServiceEditionPane {
             } else {
                 LOGGER.log(Level.WARNING, "Unexpected CSW format:{0}", this.configuration.getFormat());
             }
+            if (this.configuration.getLogLevel() != null) {
+                this.logLevelCombo.setSelectedItem(this.configuration.getLogLevel().getName());
+            }
         }
         repaint();
     }
@@ -92,6 +94,8 @@ public class JServiceCswEditPane extends JServiceEditionPane {
         guiDataSourceCombo = new JComboBox();
         centerPane = new JPanel();
         refreshIndexButton = new JButton();
+        jLabel2 = new JLabel();
+        logLevelCombo = new JComboBox();
 
         ResourceBundle bundle = ResourceBundle.getBundle("org/constellation/swing/Bundle"); // NOI18N
         jLabel1.setText(bundle.getString("sourceType")); // NOI18N
@@ -113,7 +117,7 @@ public class JServiceCswEditPane extends JServiceEditionPane {
         );
         centerPaneLayout.setVerticalGroup(
             centerPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
-            .addGap(0, 236, Short.MAX_VALUE)
+            .addGap(0, 182, Short.MAX_VALUE)
         );
 
         refreshIndexButton.setText(bundle.getString("refreshIndex")); // NOI18N
@@ -123,6 +127,10 @@ public class JServiceCswEditPane extends JServiceEditionPane {
             }
         });
 
+        jLabel2.setText(bundle.getString("logLevel")); // NOI18N
+
+        logLevelCombo.setModel(new DefaultComboBoxModel(new String[] { "INFO", "FINE", "FINER" }));
+
         GroupLayout layout = new GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -130,9 +138,12 @@ public class JServiceCswEditPane extends JServiceEditionPane {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(centerPane, GroupLayout.DEFAULT_SIZE, 442, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(centerPane, GroupLayout.DEFAULT_SIZE, 329, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(logLevelCombo, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(18, 18, 18)
                         .addComponent(refreshIndexButton))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
@@ -148,11 +159,12 @@ public class JServiceCswEditPane extends JServiceEditionPane {
                     .addComponent(jLabel1)
                     .addComponent(guiDataSourceCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
-                    .addComponent(centerPane, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(refreshIndexButton)))
+                .addComponent(centerPane, GroupLayout.DEFAULT_SIZE, 182, Short.MAX_VALUE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(refreshIndexButton)
+                    .addComponent(jLabel2)
+                    .addComponent(logLevelCombo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
@@ -181,12 +193,15 @@ public class JServiceCswEditPane extends JServiceEditionPane {
     private JPanel centerPane;
     private JComboBox guiDataSourceCombo;
     private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JComboBox logLevelCombo;
     private JButton refreshIndexButton;
     // End of variables declaration//GEN-END:variables
 
     private void updateConfiguration() {
         if (specificPane != null) {
             this.configuration = (Automatic) specificPane.getConfiguration();
+            this.configuration.setLogLevel((String)logLevelCombo.getSelectedItem());
         }
         
     }
