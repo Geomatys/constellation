f921ef42a613de17b206a94ce36b0be901fa9c02
Benjamin Garcia <Benjamin.garcia@geomatys.com>
2013-08-21 10:08:55 +0200

    generalize pagination and filter list action using attribute data-name

--- modules/gui/cstl-web-client/src/main/webapp/js/wmsedition.js
+++ modules/gui/cstl-web-client/src/main/webapp/js/wmsedition.js
@@ -2,26 +2,68 @@
  * Add action on nbLayerselect element
  * @param size : max layer number
  */
-function addLayerSelectAction(size){
-    $("#nbLayersselect").on('change', function(){
+function addLayerSelectAction(root, size){
+    $(root + " [data-name=nbLayersselect]").on('change', function(){
         var counter = $(this).val();
         generateList(0, counter, "", "");
         var nbPage = size/counter;
         generatePagination(nbPage);
-        $("#previous").addClass("active");
+        $(root+" [data-name=previous]").addClass("active");
     });
 }
 
+function addOrderByAction(root){
+    // OrderBy actions...
+    $(root + ' [data-order-by]').click(function() {
+        var orderBy   = $(this).data('order-by');
+        var counter   = $(root + " [data-name=nbLayersselect]").val();
+        var filter    = $(root + " [data-name=searchFilter]").val();
+        var direction = $(this).hasClass('descending') ? 'ascending' : 'descending'; // opposite direction
+
+        generateList(0, counter, orderBy, filter, direction);
+
+        $(this).parents('.nav').find('a').removeClass('ascending descending').
+            find('.icon-caret').removeClass('icon-caret-up icon-caret-down').hide();
+        if (direction === 'ascending') {
+            $(this).addClass('ascending').find('.icon-caret').addClass('icon-caret-up').show();
+        } else {
+            $(this).addClass('descending').find('.icon-caret').addClass('icon-caret-down').show();
+        }
+    });
+}
+
+function addFilterAction(root){
+    $(root + " [data-name=searchFilter]").keypress(function(event) {
+        var counter   = $(root + " [data-name=nbLayersselect]").val();
+        var keyCode = (event.keyCode ? event.keyCode : event.which);
+        if (keyCode == '13') {
+            generateList(0, counter, null, $(this).val(), null);
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    });
+}
+
+function addResetFilterAction(root){
+    // Reset filter action...
+    $(root + ' [data-name=resetFilters]').click(function() {
+        var counter   = $(root + " [data-name=nbLayersselect]").val();
+        $(root + " [data-name=searchFilter]").val('');
+        $(root + ' [data-order-by]').removeClass('ascending descending').
+            find('.icon-caret').removeClass('icon-caret-up icon-caret-down').hide();
+        generateList(0, counter, null, null, null);
+    });
+}
 
 /**
  * action call on pagination
  * @param pageSearched : int to know in which page we go
  * @param pageActivated : DOM element we need to activate
  */
-function changePage(pageSearched, pageActivated){
+function changePage(root, pageSearched, pageActivated){
 
     //get number element on page selected
-    var nbByPage = $("#nbLayersselect").val();
+    var nbByPage = $(root + " [data-name=nbLayersselect]").val();
 
     //compute first element on next page
     var startElement = (pageSearched-1)*nbByPage;
@@ -30,19 +72,19 @@ function changePage(pageSearched, pageActivated){
     generateList(startElement, nbByPage, "", "");
 
     //remove all style active on pagination
-    $("#paging > .active").removeClass("active");
+    $(root + " [data-name=paging] > .active").removeClass("active");
 
     //activate selected page
     $(pageActivated).addClass("active")
 
     //if it's last page, next paging is disabled
-    if(pageSearched==$("#paging > .page").length){
-        $("#next").addClass("active");
+    if(pageSearched==$(root + " [data-name=paging]> .page").length){
+        $(root + "[data-name=next]").addClass("active");
     }
 
     //if it's first page, previous paging is disabled
     if(pageSearched==1){
-        $("#previous").addClass("active");
+        $(root + "[data-name=previous]").addClass("active");
     }
 }
 
@@ -50,50 +92,50 @@ function changePage(pageSearched, pageActivated){
  * Generate pagination part when element number is change
  * @param nbPage : element number by page
  */
-function generatePagination(nbPage){
+function generatePagination(root, nbPage){
     //clear paging
-    $('#paging').empty();
+    $(root + " [data-name=paging]").empty();
 
     //if they have only one page, we don't show paging
     if(nbPage>1){
 
         //add previous button
-        $('#paging').append('<li id="previous"><a href="#" class="text-info">&laquo;</a></li>');
+        $(root + " [data-name=paging]").append('<li data-name="previous"><a href="#" class="text-info">&laquo;</a></li>');
 
         //iterate to add require pages. Activate first page by default
         for(var i=0; i<nbPage; i++){
             var page = i+1;
             if(page==1){
-                $('#paging').append('<li class="active page"><a href="#" class="text-info">'+page+'</a></li>');
+                $(root + " [data-name=paging]").append('<li class="active page"><a href="#" class="text-info">'+page+'</a></li>');
             }
             else{
-                $('#paging').append('<li class="page"><a href="#" class="text-info">'+page+'</a></li>');
+                $(root + " [data-name=paging]").append('<li class="page"><a href="#" class="text-info">'+page+'</a></li>');
             }
         }
 
         //add next button
-        $('#paging').append('<li id="next"><a href="#" class="text-info" >&raquo;</a></li>');
+        $(root + " [data-name=paging]").append('<li data-name="next"><a href="#" class="text-info" >&raquo;</a></li>');
 
         //add action on previous button
-        $("#previous").on('click', function(){
+        $(root + "[data-name=previous]").on('click', function(){
             if(!$(this).hasClass("active")){
-                var currentPage = $("#paging > .page.active").text();
+                var currentPage = $(root + " [data-name=paging] > .page.active").text();
                 var pageSearched = new Number(currentPage)-1;
-                changePage(pageSearched, $("#paging > .page").get(pageSearched-1));
+                changePage(pageSearched, $(root + " [data-name=paging] > .page").get(pageSearched-1));
             }
         });
 
         //add action on next button
-        $("#next").on('click', function(){
+        $(root + "[data-name=next]").on('click', function(){
             if(!$(this).hasClass("active")){
-                var currentPage = $("#paging > .page.active").text();
+                var currentPage = $(root + " [data-name=paging] > .page.active").text();
                 var pageSearched = new Number(currentPage)+1;
-                changePage(pageSearched, $("#paging > .page").get(pageSearched-1));
+                changePage(pageSearched, $(root + " [data-name=paging] > .page").get(pageSearched-1));
             }
         });
 
         //add action on pages
-        $("#paging > .page").on('click', function(){
+        $(root + " [data-name=paging] > .page").on('click', function(){
             if(!$(this).hasClass("active")){
                 var pageSearched = $(this).text();
                 changePage(pageSearched, this);
