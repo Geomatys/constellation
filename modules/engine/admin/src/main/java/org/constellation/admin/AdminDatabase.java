94dbe059042af4a01b9974dc0403eb175d0edc32
fbernard <devnull@localhost>
2013-10-07 11:17:13 +0200

    Fix tests.

--- modules/engine/admin/src/main/java/org/constellation/admin/AdminDatabase.java
+++ modules/engine/admin/src/main/java/org/constellation/admin/AdminDatabase.java
@@ -18,16 +18,24 @@
 package org.constellation.admin;
 
 import org.apache.sis.util.Static;
+import org.apache.sis.util.logging.Logging;
+import org.constellation.configuration.ConfigDirectory;
 import org.constellation.configuration.UserRecord;
 import org.constellation.util.DatabaseUtilities;
 import org.geotoolkit.internal.sql.DefaultDataSource;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.apache.sis.util.ArgumentChecks.ensureNonNull;
 
@@ -39,6 +47,18 @@ import static org.apache.sis.util.ArgumentChecks.ensureNonNull;
  * @since 0.9
  */
 public final class AdminDatabase extends Static {
+
+    /**
+     * Logger used for debugging and event notification.
+     */
+    private static final Logger LOGGER = Logging.getLogger(AdminDatabase.class);
+
+    /**
+     * Administration database configuration keys.
+     */
+    private static final String CONFIG_KEY_PASSWORD = "admin-db-password";
+    private static final String CONFIG_KEY_URL      = "admin-db-url";
+    private static final String CONFIG_KEY_USERNAME = "admin-db-username";
 
     /**
      * Administration database {@link DefaultDataSource} instance.
@@ -67,11 +87,15 @@ public final class AdminDatabase extends Static {
      * Obtains a administration database {@link AdminSession} instance.
      *
      * @return a {@link AdminSession} instance
-     * @throws  SQLException if a database access error occurs
+     * @throws SQLException if a database access error occurs
      */
     public static AdminSession createSession() throws SQLException {
         if (DATA_SOURCE == null) {
-            throw new IllegalStateException("Constellation administration database not configured.");
+            synchronized(AdminDatabase.class) {
+                if (DATA_SOURCE == null) {
+                    setup();
+                }
+            }
         }
         return new AdminSession(DATA_SOURCE.getConnection(USERNAME, PASSWORD), USER_CACHE);
     }
@@ -96,16 +120,40 @@ public final class AdminDatabase extends Static {
      *
      * @throws SQLException if an error occurred while connecting to database or executing a SQL statement
      */
-    public static synchronized void configure(final String dbUrl, final String username, final String password) throws SQLException {
-        ensureNonNull("dbUrl", dbUrl);
+    private static void setup() throws SQLException {
+        // Force loading driver because some container like tomcat 7.0.21+ disable drivers at startup.
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException ex) {
+            LOGGER.log(Level.WARNING, ex.getMessage(), ex);
+        }
 
-        // Update connection variables.
-        DATA_SOURCE = new DefaultDataSource(dbUrl.replace('\\','/') + ";create=true;");
-        USERNAME    = username;
-        PASSWORD    = password;
+        /*
+         * Browses Constellation configuration folder to obtain the admin database
+         * configuration. The configuration is stored into the admin-db.properties
+         * file from .constellation/admin folder. If this file is missing a default
+         * configuration file will be created during Constellation startup.
+         */
+        final Properties config = new Properties();
+        try {
+            final File configDirectory = ConfigDirectory.getAdminConfigDirectory();
+            final File configFile      = new File(configDirectory, "admin-db.properties");
 
-        // Clear user cache.
-        USER_CACHE.clear();
+            // Read or create default configuration.
+            if (configFile.exists()) {
+                config.load(new FileInputStream(configFile));
+            } else {
+                config.setProperty(CONFIG_KEY_URL, "jdbc:derby:" + configDirectory.getPath() + "/admin-db");
+                config.store(new FileOutputStream(configFile), "Auto-generated at first Constellation startup.");
+            }
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, "Unexpected exception while reading/writing administration database property file.", ex);
+        }
+
+        // Set connection variables.
+        DATA_SOURCE = new DefaultDataSource(config.getProperty(CONFIG_KEY_URL).replace('\\','/') + ";create=true;");
+        USERNAME    = config.getProperty(CONFIG_KEY_USERNAME);
+        PASSWORD    = config.getProperty(CONFIG_KEY_PASSWORD);
 
         // Establish connection and create schema if does not exist.
         AdminSession session = null;
