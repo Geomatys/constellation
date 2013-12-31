4067fedd9b7a0682fba9431d4b2bc8b25697e436
Benjamin Garcia <Benjamin.garcia@geomatys.com>
2013-12-31 11:19:15 +0100

    Test work on JSON & XML

--- modules/gui/cstl-web-client/src/test/java/org/constellation/gui/TestJerseyMarshalling.java
+++ modules/gui/cstl-web-client/src/test/java/org/constellation/gui/TestJerseyMarshalling.java
@@ -10,31 +10,33 @@ import javax.xml.bind.JAXBElement;
 import org.apache.commons.codec.binary.Base64;
 import org.constellation.dto.MetadataLists;
 import org.glassfish.jersey.client.JerseyInvocation;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.junit.Test;
 
 public class TestJerseyMarshalling {
 
-    @Test
-    public void test() {
-        // GET
-        GenericType<JAXBElement<MetadataLists>> planetType = new GenericType<JAXBElement<MetadataLists>>() {};
-        
-        final Client client = ClientBuilder.newClient();
-        final WebTarget target = client.target("http://localhost:8080");
+	@Test
+	public void test() {
+		// GET
+		GenericType<JAXBElement<MetadataLists>> planetType = new GenericType<JAXBElement<MetadataLists>>() {
+		};
 
-        //set authentication on header
-        byte[] binaryPassword = "admin:admin".getBytes();
-        final String encodedAuthentication = Base64.encodeBase64String(binaryPassword);
-        
-        //prepare builder before call get
-        JerseyInvocation.Builder builder = (JerseyInvocation.Builder) target
-        		.path("/constellation/api/1/data/metadataCodeLists/fr").request()
-		        .accept(MediaType.APPLICATION_XML_TYPE)
-		        .header("Content-Type", MediaType.APPLICATION_XML_TYPE)
-		        .header("Authorization", "Basic "+ encodedAuthentication);
-        
-        final MetadataLists planet = builder.get(planetType).getValue();
-        System.out.println("### " + planet);
-    }
+		final Client client = ClientBuilder.newClient().register(JacksonFeature.class);
+		final WebTarget target = client.target("http://localhost:8080");
+
+		// set authentication on header
+		byte[] binaryPassword = "admin:admin".getBytes();
+		final String encodedAuthentication = Base64
+				.encodeBase64String(binaryPassword);
+
+		// prepare builder before call get
+		JerseyInvocation.Builder builder = (JerseyInvocation.Builder) target
+				.path("/constellation/api/1/data/metadataCodeLists/fr")
+				.request(MediaType.APPLICATION_JSON)
+				.header("Authorization", "Basic " + encodedAuthentication);
+
+		final MetadataLists planet = builder.get(MetadataLists.class);
+		System.out.println("### " + planet);
+	}
 
 }
