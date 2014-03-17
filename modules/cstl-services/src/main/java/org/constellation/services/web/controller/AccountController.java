2b858cbc40f795af8d7f9f94be3695ea9e348262
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-03-17 10:36:59 +0100

    cstl: remove useless code

--- modules/cstl-services/src/main/java/org/constellation/services/web/controller/AccountController.java
+++ modules/cstl-services/src/main/java/org/constellation/services/web/controller/AccountController.java
@@ -1,8 +1,6 @@
 package org.constellation.services.web.controller;
 
-import org.constellation.engine.register.repository.UserRepository;
 import org.constellation.security.SecurityManagerHolder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,9 +11,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/account")
 public class AccountController {
-
-	@Autowired
-	private UserRepository userRepository;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody
