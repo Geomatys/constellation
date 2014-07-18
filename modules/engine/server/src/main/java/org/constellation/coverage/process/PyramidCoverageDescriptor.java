d60bc28f4b68109b9098ec011f01929d2b688ca9
Guilhem Legal <guilhem.legal@geomatys.com>
2014-07-18 14:30:37 +0200

    Process: add simple pyramid process

---
+++ modules/engine/server/src/main/java/org/constellation/coverage/process/PyramidCoverageDescriptor.java
@@ -0,0 +1,90 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ * Copyright 2014 Geomatys.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.constellation.coverage.process;
+
+import java.io.File;
+import org.apache.sis.util.iso.SimpleInternationalString;
+import org.constellation.process.ConstellationProcessFactory;
+import org.geotoolkit.parameter.DefaultParameterDescriptor;
+import org.geotoolkit.parameter.DefaultParameterDescriptorGroup;
+import org.geotoolkit.process.AbstractProcessDescriptor;
+import org.opengis.metadata.Metadata;
+import org.opengis.parameter.GeneralParameterDescriptor;
+import org.opengis.parameter.ParameterDescriptor;
+import org.opengis.parameter.ParameterDescriptorGroup;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.util.InternationalString;
+
+/**
+ *
+ * @author Guilhem Legal (Geomatys)
+ */
+public class PyramidCoverageDescriptor extends AbstractProcessDescriptor {
+    
+    public static final String NAME = "coverage.pyramid";
+    public static final InternationalString ABSTRACT = new SimpleInternationalString("Build a pyramid from the specified file.");
+
+
+    public static final String COVERAGE_BASE_NAME_NAME = "coverage_base_name";
+    private static final String COVERAGE_BASE_NAME_REMARKS = "the basic name of the output pyramid.";
+    public static final ParameterDescriptor<String> COVERAGE_BASE_NAME =
+            new DefaultParameterDescriptor<>(COVERAGE_BASE_NAME_NAME, COVERAGE_BASE_NAME_REMARKS, String.class, null, true);
+    
+    public static final String IMAGE_FILE_PATH_NAME = "image_file";
+    private static final String IMAGE_FILE_PATH_REMARKS = "The input image file.";
+    public static final ParameterDescriptor<String> IMAGE_FILE_PATH =
+            new DefaultParameterDescriptor<>(IMAGE_FILE_PATH_NAME, IMAGE_FILE_PATH_REMARKS, String.class, null, true);
+    
+    public static final String PYRAMID_FOLDER_NAME = "pyramid_folder";
+    private static final String PYRAMID_FOLDER_REMARKS = "The path of the folder where the pyramid will be saved.";
+    public static final ParameterDescriptor<File> PYRAMID_FOLDER =
+            new DefaultParameterDescriptor<>(PYRAMID_FOLDER_NAME, PYRAMID_FOLDER_REMARKS, File.class, null, true);
+
+    public static final String PROVIDER_OUT_ID_NAME = "output provider id";
+    private static final String PROVIDER_OUT_ID_REMARKS = "the  name of the output provider id.";
+    public static final ParameterDescriptor<String> PROVIDER_OUT_ID =
+            new DefaultParameterDescriptor<>(PROVIDER_OUT_ID_NAME, PROVIDER_OUT_ID_REMARKS, String.class, null, true);
+    
+    /**Input parameters */
+    public static final ParameterDescriptorGroup INPUT_DESC = new DefaultParameterDescriptorGroup("InputParameters",
+            new GeneralParameterDescriptor[]{COVERAGE_BASE_NAME, IMAGE_FILE_PATH, PYRAMID_FOLDER, PROVIDER_OUT_ID});
+
+    
+    public static final String PROVIDER_SOURCE_NAME = "provider_source";
+    private static final String PROVIDER_SOURCE_REMARKS = "A provider description.";
+    public static final ParameterDescriptor<ParameterValueGroup> PROVIDER_SOURCE =
+            new DefaultParameterDescriptor<>(PROVIDER_SOURCE_NAME, PROVIDER_SOURCE_REMARKS, ParameterValueGroup.class, null, true);
+    
+    /**Output parameters */
+    public static final ParameterDescriptorGroup OUTPUT_DESC = new DefaultParameterDescriptorGroup("OutputParameters",
+            new GeneralParameterDescriptor[]{PROVIDER_SOURCE});
+
+    /**
+     * Public constructor use by the ServiceRegistry to find and instantiate all ProcessDescriptor.
+     */
+    public PyramidCoverageDescriptor() {
+        super(NAME, ConstellationProcessFactory.IDENTIFICATION, ABSTRACT, INPUT_DESC, OUTPUT_DESC);
+    }
+
+    
+    @Override
+    public org.geotoolkit.process.Process createProcess(ParameterValueGroup input) {
+        return new PyramidCoverageProcess(this, input);
+    }
+}
---
+++ modules/engine/server/src/main/java/org/constellation/coverage/process/PyramidCoverageProcess.java
@@ -0,0 +1,87 @@
+/*
+ *    Constellation - An open source and standard compliant SDI
+ *    http://www.constellation-sdi.org
+ *
+ * Copyright 2014 Geomatys.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.constellation.coverage.process;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.sis.storage.DataStoreException;
+import org.constellation.coverage.PyramidCoverageHelper;
+import static org.constellation.coverage.process.PyramidCoverageDescriptor.COVERAGE_BASE_NAME;
+import static org.constellation.coverage.process.PyramidCoverageDescriptor.IMAGE_FILE_PATH;
+import static org.constellation.coverage.process.PyramidCoverageDescriptor.PROVIDER_OUT_ID;
+import static org.constellation.coverage.process.PyramidCoverageDescriptor.PROVIDER_SOURCE;
+import static org.constellation.coverage.process.PyramidCoverageDescriptor.PYRAMID_FOLDER;
+import org.constellation.process.AbstractCstlProcess;
+import org.constellation.provider.DataProviderFactory;
+import org.constellation.provider.DataProviders;
+import org.constellation.provider.configuration.ProviderParameters;
+import org.geotoolkit.coverage.xmlstore.XMLCoverageStoreFactory;
+import static org.geotoolkit.parameter.Parameters.getOrCreate;
+import static org.geotoolkit.parameter.Parameters.value;
+import org.geotoolkit.parameter.ParametersExt;
+import org.geotoolkit.process.ProcessDescriptor;
+import org.geotoolkit.process.ProcessException;
+import org.opengis.parameter.ParameterValueGroup;
+import org.opengis.referencing.operation.TransformException;
+import org.opengis.util.FactoryException;
+
+/**
+ *
+ * @author Guilhem Legal (Geomatys)
+ */
+public class PyramidCoverageProcess extends AbstractCstlProcess {
+
+    public PyramidCoverageProcess(final ProcessDescriptor desc, final ParameterValueGroup parameter) {
+        super(desc, parameter);
+    }
+
+    @Override
+    protected void execute() throws ProcessException {
+        final String providerID       = value(PROVIDER_OUT_ID, inputParameters);
+        final String imageFilePath    = value(IMAGE_FILE_PATH, inputParameters);
+        final String coverageBaseName = value(COVERAGE_BASE_NAME, inputParameters);
+        final File pyramidFolder      = value(PYRAMID_FOLDER, inputParameters);
+        try {
+            PyramidCoverageHelper pyramidHelper = PyramidCoverageHelper.builder(coverageBaseName).
+                    inputFormat("AUTO").withDeeps(new double[]{1}).
+                    fromImage(imageFilePath).toFileStore(pyramidFolder.getAbsolutePath()).build();
+            pyramidHelper.buildPyramid(null);
+        } catch (DataStoreException | TransformException | FactoryException | MalformedURLException ex) {
+            throw new ProcessException("Error while building pyramid", this, ex);
+        }
+        
+        try {
+            final DataProviderFactory factory = DataProviders.getInstance().getFactory("coverage-store");
+            final ParameterValueGroup pparams = factory.getProviderDescriptor().createValue();
+            ParametersExt.getOrCreateValue(pparams, ProviderParameters.SOURCE_ID_DESCRIPTOR.getName().getCode()).setValue(providerID);
+            ParametersExt.getOrCreateValue(pparams, ProviderParameters.SOURCE_TYPE_DESCRIPTOR.getName().getCode()).setValue("coverage-store");
+            final ParameterValueGroup choiceparams = ParametersExt.getOrCreateGroup(pparams, factory.getStoreDescriptor().getName().getCode());
+            final ParameterValueGroup xmlpyramidparams = ParametersExt.getOrCreateGroup(choiceparams, XMLCoverageStoreFactory.PARAMETERS_DESCRIPTOR.getName().getCode());
+            ParametersExt.getOrCreateValue(xmlpyramidparams, XMLCoverageStoreFactory.PATH.getName().getCode()).setValue(pyramidFolder.toURL());
+            ParametersExt.getOrCreateValue(xmlpyramidparams, XMLCoverageStoreFactory.NAMESPACE.getName().getCode()).setValue("no namespace");
+            getOrCreate(PROVIDER_SOURCE, outputParameters).setValue(pparams);
+        } catch (MalformedURLException ex) {
+            throw new ProcessException("the pyramid folder path is malformed", this, ex);
+        }
+    } 
+    
+}
