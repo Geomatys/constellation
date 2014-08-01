eff3666196ec4c65dc516aa987949c166c9ddfe0
Christophe MOURETTE <christophe.mourette@geomatys.com>
2014-08-01 14:50:31 +0200

    add injection declaration for test

--- modules/xml-bindings/configuration/src/main/java/org/constellation/utils/CstlMetadatas.java
+++ modules/xml-bindings/configuration/src/main/java/org/constellation/utils/CstlMetadatas.java
@@ -37,28 +37,28 @@ import static org.constellation.utils.CstlMetadataTemplate.SERVICE;
  */
 public class CstlMetadatas {
     
-    /**
-     * Create a default metadata for OGC service.
-     *
-     * @param serviceIdentifier the identifier of the service.
-     * @param serviceType the OGC type of the service (WMS, SOS, WFS, ...)
-     * @param cstlURL the current URL of the service.
-     * @param serviceInfo Information about the service.
-     * 
-     * @return DefaultMetadata a new metadata for the service with given identifier.
-     */
-    public static DefaultMetadata defaultServiceMetadata(final String serviceIdentifier, final String serviceType, final String cstlURL, final Details serviceInfo) {
-        final String serviceID = getMetadataIdForService(serviceIdentifier, serviceType);
-        final DefaultMetadata metadata = defaultServiceMetadata(serviceID, serviceInfo);
-        
-        // add specific service part
-        final MetadataFeeder feeder = new MetadataFeeder(metadata);
-        final String serviceURL = cstlURL + "/WS/" + serviceType.toLowerCase() + '/' + serviceIdentifier;
-        feeder.feedService(serviceInfo);
-        feeder.addServiceInformation(serviceType, serviceURL);
-        feeder.setServiceInstanceName(serviceIdentifier);
-        return metadata;
-    }
+//    /**
+//     * Create a default metadata for OGC service.
+//     *
+//     * @param serviceIdentifier the identifier of the service.
+//     * @param serviceType the OGC type of the service (WMS, SOS, WFS, ...)
+//     * @param cstlURL the current URL of the service.
+//     * @param serviceInfo Information about the service.
+//     *
+//     * @return DefaultMetadata a new metadata for the service with given identifier.
+//     */
+//    public static DefaultMetadata defaultServiceMetadata(final String serviceIdentifier, final String serviceType, final String cstlURL, final Details serviceInfo) {
+//        final String serviceID = getMetadataIdForService(serviceIdentifier, serviceType);
+//        final DefaultMetadata metadata = defaultServiceMetadata(serviceID, serviceInfo);
+//
+//        // add specific service part
+//        final MetadataFeeder feeder = new MetadataFeeder(metadata);
+//        final String serviceURL = cstlURL + "/WS/" + serviceType.toLowerCase() + '/' + serviceIdentifier;
+//        feeder.feedService(serviceInfo);
+//        feeder.addServiceInformation(serviceType, serviceURL);
+//        feeder.setServiceInstanceName(serviceIdentifier);
+//        return metadata;
+//    }
     
     public static void updateServiceMetadataURL(final String serviceIdentifier, final String serviceType, final String cstlURL, final DefaultMetadata metadata) {
         final MetadataFeeder feeder = new MetadataFeeder(metadata);
@@ -66,64 +66,64 @@ public class CstlMetadatas {
         feeder.updateServiceURL(serviceURL);
     }
     
-    public static void updateServiceMetadataLayer(final DefaultMetadata metadata, final List<String> layerIds) {
-        final MetadataFeeder feeder = new MetadataFeeder(metadata);
-        feeder.setServiceMetadataIdForData(layerIds);
-    }
+//    public static void updateServiceMetadataLayer(final DefaultMetadata metadata, final List<String> layerIds) {
+//        final MetadataFeeder feeder = new MetadataFeeder(metadata);
+//        feeder.setServiceMetadataIdForData(layerIds);
+//    }
     
-    public static void addServiceMetadataLayer(final DefaultMetadata metadata, final String layerId) {
-        final MetadataFeeder feeder = new MetadataFeeder(metadata);
-        feeder.addServiceMetadataIdForData(layerId);
-    }
+//    public static void addServiceMetadataLayer(final DefaultMetadata metadata, final String layerId) {
+//        final MetadataFeeder feeder = new MetadataFeeder(metadata);
+//        feeder.addServiceMetadataIdForData(layerId);
+//    }
     
-    public static String getMetadataIdForService(final String serviceName, final String serviceType){
-        ArgumentChecks.ensureNonNull("serviceName", serviceName);
-        ArgumentChecks.ensureNonNull("serviceType", serviceType);
-        return SERVICE.getPrefix()+ '_' + serviceType + "_" + serviceName;
-    }
+//    public static String getMetadataIdForService(final String serviceName, final String serviceType){
+//        ArgumentChecks.ensureNonNull("serviceName", serviceName);
+//        ArgumentChecks.ensureNonNull("serviceType", serviceType);
+//        return serviceType + "_" + serviceName;
+//    }
     
-    public static String getMetadataIdForProvider(final String providerId){
-        ArgumentChecks.ensureNonNull("providerId", providerId);
-        return PROVIDER.getPrefix()+ '_' + providerId;
-    }
+//    public static String getMetadataIdForProvider(final String providerId){
+//        ArgumentChecks.ensureNonNull("providerId", providerId);
+//        return PROVIDER.getPrefix()+ '_' + providerId;
+//    }
     
     public static String getMetadataIdForData(final String providerId, final Name dataName){
         ArgumentChecks.ensureNonNull("dataName", dataName);
         ArgumentChecks.ensureNonNull("providerId", providerId);
-        return DATA.getPrefix()+ '_' + providerId + '_' + dataName.getLocalPart(); // TODO namespace?
+        return  providerId + '_' + dataName.getLocalPart(); // TODO namespace?
     }
     
-    public static String getMetadataIdForLayer(final String dataName){
-        ArgumentChecks.ensureNonNull("dataName", dataName);
-        return DATA.getPrefix()+ '_' + dataName;
-    }
+//    public static String getMetadataIdForLayer(final String dataName){
+//        ArgumentChecks.ensureNonNull("dataName", dataName);
+//        return DATA.getPrefix()+ '_' + dataName;
+//    }
     
-    /**
-     * Create a default metadata for the given data.
-     *
-     * @param metadataId the identifier of the metadata to create.
-     * @param serviceInfo
-     * 
-     * @return A new {@link DefaultMetadata}. ISO_19115 compliant.
-     */
-    private static DefaultMetadata defaultServiceMetadata(final String metadataId, final Details serviceInfo) {
-
-        ArgumentChecks.ensureNonNull("serviceInfo", serviceInfo);
-        ArgumentChecks.ensureNonNull("metadataId", metadataId);
-        final DefaultMetadata metadata = new DefaultMetadata();
-        final MetadataFeeder feeder    = new MetadataFeeder(metadata);
-        final Date creationDate        = new Date();
-
-        // create the Service part
-        feeder.getServiceIdentification();
-        feeder.setTitle(serviceInfo.getName());
-        feeder.setCitationIdentifier(metadataId);
-        feeder.setCreationDate(creationDate);
-
-        // Fills metadata with generic values
-        metadata.setFileIdentifier(metadataId);
-        metadata.setDateStamp(creationDate);
-
-        return metadata;
-    }
+//    /**
+//     * Create a default metadata for the given data.
+//     *
+//     * @param metadataId the identifier of the metadata to create.
+//     * @param serviceInfo
+//     *
+//     * @return A new {@link DefaultMetadata}. ISO_19115 compliant.
+//     */
+//    private static DefaultMetadata defaultServiceMetadata(final String metadataId, final Details serviceInfo) {
+//
+//        ArgumentChecks.ensureNonNull("serviceInfo", serviceInfo);
+//        ArgumentChecks.ensureNonNull("metadataId", metadataId);
+//        final DefaultMetadata metadata = new DefaultMetadata();
+//        final MetadataFeeder feeder    = new MetadataFeeder(metadata);
+//        final Date creationDate        = new Date();
+//
+//        // create the Service part
+//        feeder.getServiceIdentification();
+//        feeder.setTitle(serviceInfo.getName());
+//        feeder.setCitationIdentifier(metadataId);
+//        feeder.setCreationDate(creationDate);
+//
+//        // Fills metadata with generic values
+//        metadata.setFileIdentifier(metadataId);
+//        metadata.setDateStamp(creationDate);
+//
+//        return metadata;
+//    }
 }
