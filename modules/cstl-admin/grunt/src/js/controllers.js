06a7d45360c9b0b4cd26d68e622f979257fe4611
msidhoum <mehdi.sidhoum@geomatys.com>
2014-08-20 18:22:23 +0200

    Fix user's profile page and language switcher for Chrome browser.

--- modules/cstl-admin/grunt/src/js/controllers.js
+++ modules/cstl-admin/grunt/src/js/controllers.js
@@ -69,8 +69,11 @@ cstlAdminApp.controller('MainController', ['$scope','$location','webService','da
 
 cstlAdminApp.controller('LanguageController', ['$scope', '$translate',
     function ($scope, $translate) {
-        $scope.changeLanguage = function (languageKey) {
-            $translate.use(languageKey);
+
+        $scope.currentLang = 'en';
+
+        $scope.changeLanguage = function () {
+            $translate.use($scope.currentLang);
         };
     }]);
 
