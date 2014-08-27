9c90e1d1aa83ec6d3e9df7f02579f509b827760a
msidhoum <mehdi.sidhoum@geomatys.com>
2014-08-27 15:21:51 +0200

    Fix codelists for characterSetCodes, using java CharSet.

--- modules/web-services/restfull/src/main/java/org/constellation/rest/api/DataRest.java
+++ modules/web-services/restfull/src/main/java/org/constellation/rest/api/DataRest.java
@@ -154,6 +154,7 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -282,11 +283,16 @@ public class DataRest {
 
         // for characterSet codes
         final List<String> characterSetCodes = new LinkedList<>();
-        for (final CharacterSet c : CharacterSet.values()) {
-            final String standardName = Types.getStandardName(c.getClass());
-            final String code = c.identifier()!=null? c.identifier(): c.name();
-            final String codeListName = standardName+"."+code;
-            characterSetCodes.add(codeListName);
+        final Set<String> keys = Charset.availableCharsets().keySet();
+        final List<String> keep = Arrays.asList("UTF-8","UTF-16","UTF-32",
+                "ISO-8859-1","ISO-8859-13","ISO-8859-15",
+                "ISO-8859-2","ISO-8859-3","ISO-8859-4",
+                "ISO-8859-5","ISO-8859-6","ISO-8859-7",
+                "ISO-8859-8","ISO-8859-9","Shift_JIS",
+                "EUC-JP","EUC-KR","US-ASCII","Big5","GB2312");
+        keep.retainAll(keys);
+        for (final String c : keep) {
+            characterSetCodes.add(c);
         }
         Collections.sort(characterSetCodes);
         mdList.setCharacterSetCodes(characterSetCodes);
