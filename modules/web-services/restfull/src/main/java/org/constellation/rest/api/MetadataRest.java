16298fd037d9c83de47c5fd073bffcb9c2986eab
msidhoum <mehdi.sidhoum@geomatys.com>
2014-09-04 13:05:41 +0200

    CSTL-801 working on Metadata rest api to serve client side, adding method that returns all dataset with its children.

--- modules/web-services/restfull/src/main/java/org/constellation/rest/api/MetadataRest.java
+++ modules/web-services/restfull/src/main/java/org/constellation/rest/api/MetadataRest.java
@@ -19,19 +19,29 @@
 
 package org.constellation.rest.api;
 
+import com.google.common.base.Optional;
 import org.apache.sis.util.logging.Logging;
+import org.constellation.admin.DataBusiness;
+import org.constellation.admin.DatasetBusiness;
 import org.constellation.admin.MetadataBusiness;
+import org.constellation.admin.ProviderBusiness;
+import org.constellation.configuration.DataBrief;
+import org.constellation.configuration.DataSetBrief;
+import org.constellation.engine.register.CstlUser;
+import org.constellation.engine.register.Dataset;
+import org.constellation.engine.register.Provider;
+import org.constellation.engine.register.repository.UserRepository;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
-import static org.constellation.utils.RESTfulUtilities.ok;
-
 /**
- * RESTful API for metadata.
+ * RESTful API for dataset metadata.
  *
  * @author Mehdi Sidhoum (Geomatys).
  * @version 0.9
@@ -42,17 +52,69 @@ import static org.constellation.utils.RESTfulUtilities.ok;
 @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
 public class MetadataRest {
 
+    /**
+     * Used for debugging purposes.
+     */
     private static final Logger LOGGER = Logging.getLogger(MetadataRest.class);
 
+    /**
+     * Injected metadata business.
+     */
     @Inject
     private MetadataBusiness metadataBusiness;
 
+    /**
+     * Injected provider business.
+     */
+    @Inject
+    private ProviderBusiness providerBusiness;
+
+    /**
+     * Injected dataset business.
+     */
+    @Inject
+    private DatasetBusiness datasetBusiness;
+
+    /**
+     * Injected data business.
+     */
+    @Inject
+    private DataBusiness dataBusiness;
+
+    /**
+     * Injected user repository.
+     */
+    @Inject
+    private UserRepository userRepository;
+
+    /**
+     * Returns all dataset in brief format with its own list of data in brief format.
+     *
+     * @return Response for client side as json.
+     */
     @GET
-    @Path("all/{includeService}")
-    public Response getAllMetadata(final @PathParam("includeService") boolean includeService) throws Exception {
-        //@TODO we need to return a minimized object instead of the entire metadata
-        //@see what is needed by the dashboard page.
-        return ok(metadataBusiness.getAllMetadata(includeService));
+    @Path("dataset/all")
+    public Response getAllDataset() {
+        final List<DataSetBrief> datasetBriefs = new ArrayList<>();
+        final List<Dataset> datasets = datasetBusiness.getAllDataset();
+        if(datasets!=null){
+            for(final Dataset ds : datasets){
+                final Integer dataSetId = ds.getId();
+                final Provider provider = providerBusiness.getProvider(ds.getProviderId());
+                final Optional<CstlUser> optUser = userRepository.findById(provider.getOwner());
+                String owner = null;
+                if(optUser!=null){
+                    final CstlUser user = optUser.get();
+                    if(user != null){
+                        owner = user.getLogin();
+                    }
+                }
+                final List<DataBrief> dataBriefList = dataBusiness.getDataBriefsFromDatasetId(dataSetId);
+                final DataSetBrief dsb = new DataSetBrief(ds.getIdentifier(), provider.getType(), owner, dataBriefList);
+                datasetBriefs.add(dsb);
+            }
+        }
+        return Response.ok(datasetBriefs).build();
     }
 
 }
