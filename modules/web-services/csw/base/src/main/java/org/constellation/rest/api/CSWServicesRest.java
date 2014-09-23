ee8071960b3020a4e2bda774c7cc55d2dbb46ecd
msidhoum <mehdi.sidhoum@geomatys.com>
2014-09-23 14:55:13 +0200

    CSW api rest adding method to save metadata. (todo for internal)

--- modules/web-services/csw/base/src/main/java/org/constellation/rest/api/CSWServicesRest.java
+++ modules/web-services/csw/base/src/main/java/org/constellation/rest/api/CSWServicesRest.java
@@ -29,6 +29,7 @@ import org.constellation.dto.ParameterValues;
 import org.constellation.dto.SimpleValue;
 import org.constellation.generic.database.Automatic;
 import org.constellation.json.metadata.Template;
+import org.constellation.json.metadata.binding.RootObj;
 import org.constellation.metadata.CSWworker;
 import org.constellation.metadata.configuration.CSWConfigurer;
 import org.constellation.utils.ISOMarshallerPool;
@@ -205,7 +206,7 @@ public class CSWServicesRest {
      *
      * @param id service identifier.
      * @param metaID given record identifier.
-     * @param type sensor type system or component.
+     * @param type type raster or vector.
      * @param prune flag that indicates if template result will clean empty children/block.
      * @return {@code Response}
      */
@@ -214,9 +215,9 @@ public class CSWServicesRest {
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes({MediaType.APPLICATION_JSON})
     public Response getCSWMetadataJson(final @PathParam("id") String id,
-                                          final @PathParam("metaID") String metaID,
-                                          final @PathParam("type") String type,
-                                          final @PathParam("prune") boolean prune) {
+                                       final @PathParam("metaID") String metaID,
+                                       final @PathParam("type") String type,
+                                       final @PathParam("prune") boolean prune) {
         try{
             final Node node = getConfigurer().getMetadata(id, metaID);
             if(node!=null){
@@ -255,6 +256,54 @@ public class CSWServicesRest {
         return (CSWConfigurer) ServiceConfigurer.newInstance(Specification.CSW);
     }
 
+    /**
+     * Proceed to save metadata with given values from metadata editor.
+     *
+     * @param id service identifier.
+     * @param metaID given record identifier.
+     * @param type the data type.
+     * @param metadataValues the values of metadata editor.
+     * @return {@code Response}
+     */
+    @POST
+    @Path("{id}/metadata/save/{metaID}/{type}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response saveMetadata(final @PathParam("id") String id,
+                                  final @PathParam("metaID") String metaID,
+                                  final @PathParam("type") String type,
+                                  final RootObj metadataValues) {
+        try {
+            final Node node = getConfigurer().getMetadata(id, metaID);
+            if(node!=null){
+                final Metadata metadata = getMetadataFromNode(node);
+                if(metadata!=null){
+                    //get template name
+                    final String templateName;
+                    if ("vector".equalsIgnoreCase(type)) {
+                        //vector template
+                        templateName = "profile_default_vector";
+                    } else if ("raster".equalsIgnoreCase(type)){
+                        //raster template
+                        templateName = "profile_default_raster";
+                    }else {
+                        //default template is import
+                        templateName = "profile_import";
+                    }
+                    final Template template = Template.getInstance(templateName);
+                    template.read(metadataValues,metadata,false);
+                    // Save metadata
+                    final Node result = getNodeFromGeotkMetadata(metadata);
+                    //@TODO call updateRecord instead
+                    getConfigurer().importRecord(id,result);
+                }
+            }
+        } catch (Exception ex) {
+            LOGGER.log(Level.WARNING, "Error while saving metadata", ex);
+            return Response.status(500).entity(ex.getLocalizedMessage()).build();
+        }
+        return Response.ok().type(MediaType.TEXT_PLAIN_TYPE).build();
+    }
 
     /**
      * Convert {@code Metadata} to {@code Node} object.
