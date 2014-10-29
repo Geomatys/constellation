ea36b569ffcc743d8dce53b3d934f38479a27045
msidhoum <mehdi.sidhoum@geomatys.com>
2014-10-29 17:48:55 +0100

    WMTS viewer : starting works to init the map.

--- modules/cstl-admin/grunt/src/js/cstl.viewer.wmts.js
+++ modules/cstl-admin/grunt/src/js/cstl.viewer.wmts.js
@@ -16,30 +16,84 @@
 window.WmtsViewer = {
     map : undefined,
     target : undefined,
-    layers : [],
-    extent: [-180, -90, 180, 90],
-    projection: 'EPSG:4326',
-    maxExtent : undefined,
+    layers : [], //array of layers to display
+    extent: [-180, -85, 180, 85], //extent array of coordinates always in 4326
+    projection: 'EPSG:3857', //given projection used to display layers
+    maxExtent : undefined, //the maximum extent for the given projection
+    addBackground : true,
 
     initMap : function(mapId){
+        //unbind the old map
         if (WmtsViewer.map) {
             WmtsViewer.map.setTarget(undefined);
         }
+
+        //get projection extent
+        var projection = ol.proj.get(WmtsViewer.projection);
+        WmtsViewer.maxExtent = projection.getExtent();
+        //calculate reprojected extent for given projection
+        var reprojExtent = ol.proj.transform(WmtsViewer.extent, 'EPSG:4326', WmtsViewer.projection);
+        //if the projected extent contains Infinity then the extent will be the projection extent.
+        if(Array.isArray(reprojExtent)){
+            for(var i=0;i<reprojExtent.length;i++){
+                var coord = reprojExtent[i];
+                if(WmtsViewer.isNotNumber(coord)) {
+                    reprojExtent = projection.getExtent();
+                    break;
+                }
+            }
+        }
+
+        if(WmtsViewer.addBackground) {
+            //adding background layer by default OSM
+            var sourceOSM = new ol.source.OSM({
+                attributions:[]
+            });
+            var backgroundLayer = new ol.layer.Tile({
+                source: sourceOSM
+            });
+            WmtsViewer.layers.unshift(backgroundLayer);
+        }
+
         WmtsViewer.map = new ol.Map({
+            controls: ol.control.defaults().extend([
+                new ol.control.ScaleLine({
+                    units: 'metric'
+                })
+            ]),
             layers: WmtsViewer.layers,
             target: mapId,
             view: new ol.View({
                 projection: WmtsViewer.projection,
-                extent: WmtsViewer.extent
+                extent: reprojExtent
             }),
             logo: false
         });
-        var projection = ol.proj.get(WmtsViewer.projection);
-        WmtsViewer.maxExtent = projection.getExtent();
 
         // Zoom on specified extent
         WmtsViewer.map.updateSize();
-        WmtsViewer.map.getView().fitExtent(WmtsViewer.extent, WmtsViewer.map.getSize());
+        var size = WmtsViewer.map.getSize();
+        WmtsViewer.map.getView().fitExtent(reprojExtent, size);
+    },
+
+    isNotNumber : function(n) {
+        return (n === Number.POSITIVE_INFINITY || n === Number.NEGATIVE_INFINITY || isNaN(n));
+    },
+
+    zoomToExtent : function(extent,size){
+        var projection = ol.proj.get(WmtsViewer.projection);
+        var reprojExtent = ol.proj.transform(extent, 'EPSG:4326', WmtsViewer.projection);
+        if(Array.isArray(reprojExtent)){
+            for(var i=0;i<reprojExtent.length;i++){
+                var coord = reprojExtent[i];
+                if(WmtsViewer.isNotNumber(coord)){
+                    reprojExtent = projection.getExtent();
+                    break;
+                }
+            }
+        }
+        WmtsViewer.map.getView().fitExtent(reprojExtent, size);
+        WmtsViewer.map.getView().setZoom(WmtsViewer.map.getView().getZoom()+1);
     },
 
     createLayer : function(layerName, instance, capabilities){
