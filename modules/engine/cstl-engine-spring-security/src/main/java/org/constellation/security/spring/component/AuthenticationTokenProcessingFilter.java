9a3f26733d74b07ab1e1acc2e6de048867d0634d
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-11-06 17:13:48 +0100

    Also look for token from cookie.

--- modules/engine/cstl-engine-spring-security/src/main/java/org/constellation/security/spring/component/AuthenticationTokenProcessingFilter.java
+++ modules/engine/cstl-engine-spring-security/src/main/java/org/constellation/security/spring/component/AuthenticationTokenProcessingFilter.java
@@ -1,14 +1,19 @@
 package org.constellation.security.spring.component;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.constellation.security.spring.TokenService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,75 +24,87 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
 
-
 @Component
-public class AuthenticationTokenProcessingFilter extends GenericFilterBean
-{
+public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
+
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger("token");
+    
+    @Autowired
+    private UserDetailsService userService;
 
     @Autowired
-	private UserDetailsService userService;
+    private TokenService tokenService;
 
-	@Autowired
-	private TokenService tokenService;
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = this.getAsHttpRequest(request);
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
-			ServletException
-	{
-		HttpServletRequest httpRequest = this.getAsHttpRequest(request);
+        String authToken = this.extractAuthTokenFromRequest(httpRequest);
+        String userName = tokenService.getUserNameFromToken(authToken);
 
-		String authToken = this.extractAuthTokenFromRequest(httpRequest);
-		String userName = tokenService.getUserNameFromToken(authToken);
+        if (userName != null) {
 
-		if (userName != null) {
+            UserDetails userDetails = this.userService.loadUserByUsername(userName);
 
-			UserDetails userDetails = this.userService.loadUserByUsername(userName);
+            if (tokenService.validateToken(authToken, userDetails.getUsername())) {
 
-			if (tokenService.validateToken(authToken, userDetails.getUsername())) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+                        userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
 
-				UsernamePasswordAuthenticationToken authentication =
-						new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-			}
-		}
+        chain.doFilter(request, response);
+    }
 
-		chain.doFilter(request, response);
-	}
+    private HttpServletRequest getAsHttpRequest(ServletRequest request) {
+        if (!(request instanceof HttpServletRequest)) {
+            throw new RuntimeException("Expecting an HTTP request");
+        }
 
+        return (HttpServletRequest) request;
+    }
 
-	private HttpServletRequest getAsHttpRequest(ServletRequest request)
-	{
-		if (!(request instanceof HttpServletRequest)) {
-			throw new RuntimeException("Expecting an HTTP request");
-		}
+    private String extractAuthTokenFromRequest(HttpServletRequest httpRequest) {
+        /* Get token from header */
+        String authToken = httpRequest.getHeader("X-Auth-Token");
+        if (authToken != null) {
+            LOGGER.debug("Header: " + authToken + " (" + httpRequest.getRequestURI() + ")");
+            return authToken;
+        }
 
-		return (HttpServletRequest) request;
-	}
+        /* Extract from cookie */
+        Cookie[] cookies = httpRequest.getCookies();
+        if(cookies!=null)
+        for (Cookie cookie : cookies) {
+            if("authToken".equals(cookie.getName())) {
+                try {
+                    LOGGER.debug("Cookie: " + authToken + " (" + httpRequest.getRequestURI() + ")");
+                    return URLDecoder.decode(cookie.getValue(), "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    LOGGER.error(e.getMessage(), e);
+                }
+            }
+        }
+        
+        /* If token not found get it from request query string 'token' parameter */
+        String queryString = httpRequest.getQueryString();
+        if (StringUtils.hasText(queryString)) {
+            int tokenIndex = queryString.indexOf("token=");
+            if (tokenIndex != -1) {
+                tokenIndex += "token=".length();
+                int tokenEndIndex = queryString.indexOf('&', tokenIndex);
+                if (tokenEndIndex == -1)
+                    authToken = queryString.substring(tokenIndex);
+                else
+                    authToken = queryString.substring(tokenIndex, tokenEndIndex);
+                LOGGER.debug("QueryString: " + authToken + " (" + httpRequest.getRequestURI() + ")");
+            }
+        }
+        // authToken = httpRequest.getParameter("token");
 
-
-	private String extractAuthTokenFromRequest(HttpServletRequest httpRequest)
-	{
-		/* Get token from header */
-		String authToken = httpRequest.getHeader("X-Auth-Token");
-
-		/* If token not found get it from request query string 'token' parameter */
-		if (authToken == null) {
-		    String queryString = httpRequest.getQueryString();
-		    if(StringUtils.hasText(queryString)) {
-		        int tokenIndex = queryString.indexOf("token=");
-		        if(tokenIndex!=-1) {
-		            tokenIndex+="token=".length();
-		            int tokenEndIndex = queryString.indexOf('&', tokenIndex);
-		            if(tokenEndIndex==-1)
-		                authToken = queryString.substring(tokenIndex);
-		            else
-		                authToken = queryString.substring(tokenIndex, tokenEndIndex);
-		        }
-		    }
-			//authToken = httpRequest.getParameter("token");
-		}
-
-		return authToken;
-	}
+        return authToken;
+    }
 }
