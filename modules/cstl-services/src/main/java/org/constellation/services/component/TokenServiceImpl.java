0362d6db380add2e0ac9f3b3aab3c570b9720071
Olivier Nouguier <olivier.nouguier@geomatys.com>
2014-11-12 18:00:09 +0100

    Injection of secret (token)

--- modules/cstl-services/src/main/java/org/constellation/services/component/TokenServiceImpl.java
+++ modules/cstl-services/src/main/java/org/constellation/services/component/TokenServiceImpl.java
@@ -1,13 +1,27 @@
 package org.constellation.services.component;
 
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
 import org.constellation.security.spring.TokenService;
 import org.constellation.token.TokenUtils;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TokenServiceImpl implements TokenService {
 
     private String secret = "cstl-sdi";
+    
+    @Inject
+    private Environment env;
+    
+    @PostConstruct
+    public void init() {
+        secret = env.getProperty("cstl.secret", UUID.randomUUID().toString());
+    }
     
     public String createToken(String username) {
         return TokenUtils.createToken(username, secret);
