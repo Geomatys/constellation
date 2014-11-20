e92a7adaf71a191be89cebdf71a5f72664e40a50
msidhoum <mehdi.sidhoum@geomatys.com>
2014-11-20 14:02:51 +0100

    Fix login.js the startsWith function is not available for Chrome browser.

--- modules/cstl-admin/grunt/src/js/login.js
+++ modules/cstl-admin/grunt/src/js/login.js
@@ -68,7 +68,7 @@ cstlLoginApp.controller("login", function($scope, $http){
 
     $http.get('app/conf', {isArray: false}).success(function(conf){
         cstlUrl = conf.cstl;
-        if(!cstlUrl.startsWith('http')){
+        if(cstlUrl.indexOf('http://')===-1){
             var currentUrl = window.location.href;
             cstlUrl = currentUrl.substring(0,currentUrl.indexOf('/',7))+cstlUrl;
         }
