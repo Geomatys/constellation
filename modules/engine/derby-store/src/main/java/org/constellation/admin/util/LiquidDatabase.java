e8a404e869ea05a05d33b7595b291963ff1fcbba
qboileau <quentin.boileau@geomatys.com>
2015-01-13 14:16:10 +0100

    DB : force derby driver loading in classpath before try to build temporary database.

--- modules/engine/derby-store/src/main/java/org/constellation/admin/util/LiquidDatabase.java
+++ modules/engine/derby-store/src/main/java/org/constellation/admin/util/LiquidDatabase.java
@@ -1,21 +1,25 @@
 package org.constellation.admin.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
-
 import org.apache.commons.io.FileUtils;
+import org.apache.sis.util.logging.Logging;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LiquidDatabase {
+
+    private static final Logger LOGGER = Logging.getLogger(LiquidDatabase.class);
 
     public static void main(String[] args) throws IOException {
 
@@ -24,6 +28,12 @@ public class LiquidDatabase {
 
         if (!path.startsWith("/tmp/") && !path.contains("target/"))
             throw new IOException("tmp files must be located in /tmp or target folder: " + path);
+
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+        } catch (ClassNotFoundException ex) {
+            LOGGER.log(Level.WARNING, ex.getMessage(), ex);
+        }
 
         File dbDir = new File(path);
         if (dbDir.exists()) {
