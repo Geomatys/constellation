122e98ab32fd0164bf4c0bc23a8842ad44d77a4c
qboileau <quentin.boileau@geomatys.com>
2015-03-25 16:42:28 +0100

    fix(REST) : fix extractWMTSLayerInfo() analysis.
    Input crs parameter is now used to find matching TileMatrixSet and search/reproject dataExtent value from layer BoundingBoxType.

--- modules/web-services/map/src/main/java/org/constellation/rest/api/MapRest.java
+++ modules/web-services/map/src/main/java/org/constellation/rest/api/MapRest.java
@@ -19,6 +19,10 @@
 
 package org.constellation.rest.api;
 
+import org.apache.sis.geometry.Envelopes;
+import org.apache.sis.geometry.GeneralEnvelope;
+import org.apache.sis.referencing.crs.AbstractCRS;
+import org.apache.sis.referencing.cs.AxesConvention;
 import org.apache.sis.util.logging.Logging;
 import org.constellation.admin.exception.ConstellationException;
 import org.constellation.business.IDataBusiness;
@@ -40,7 +44,11 @@ import org.geotoolkit.ows.xml.v110.WGS84BoundingBoxType;
 import org.geotoolkit.referencing.CRS;
 import org.geotoolkit.wmts.WMTSUtilities;
 import org.geotoolkit.wmts.xml.v100.*;
+import org.opengis.geometry.Envelope;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+import org.opengis.util.FactoryException;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -174,6 +182,22 @@ public class MapRest {
         return Response.ok().type(MediaType.TEXT_PLAIN_TYPE).build();
     }
 
+    /**
+     * This method will analyse a getCapabilities and extract information for a specific layer and crs.
+     * Returned map will contain :
+     * <ul>
+     *     <li>"matrixSet" : Tile matrix set id matching input layer name and crs</li>
+     *     <li>"matrixIds" : list of tile matrices identifier in matrixSet</li>
+     *     <li>"resolutions" : list of resolutions converted in pixel unit see
+     *     {@link WMTSUtilities#unitsByPixel(org.geotoolkit.wmts.xml.v100.TileMatrixSet, org.opengis.referencing.crs.CoordinateReferenceSystem, org.geotoolkit.wmts.xml.v100.TileMatrix)}</li>
+     *     <li>"style" : default style of the layer </li>
+     *     <li>"dataExtent" : layer bbox in requested CRS with <b>longitude first forced</b></li>
+     * </ul>
+     * TODO should we move code logic business layer ?
+     * @param crs CRS code.
+     * @return a map wrapped in Response object
+     * @throws ConfigurationException
+     */
     @POST
     @Path("{id}/extractLayerInfo/{layerName}/{crs}")
     @Produces({MediaType.APPLICATION_JSON})
@@ -183,13 +207,15 @@ public class MapRest {
                                          final @PathParam("layerName") String layerName,
                                          final @PathParam("crs") String crs,
                                          final Capabilities capabilities) throws ConfigurationException {
+
         final Map<String,Object> map = new HashMap<>();
-        if(capabilities != null){
+        if (capabilities != null) {
             final ContentsType contents = capabilities.getContents();
-            if(contents != null){
-                final List<LayerType> layertypeList = contents.getLayers();
+            if (contents != null) {
+                //find layer in capabilities
+                final List<LayerType> layerTypeList = contents.getLayers();
                 LayerType layerType = null;
-                for(final LayerType lt : layertypeList){
+                for(final LayerType lt : layerTypeList){
                     if(layerName.equals(lt.getIdentifier().getValue())){
                         layerType = lt;
                         break;
@@ -198,64 +224,186 @@ public class MapRest {
                 if(layerType == null) {
                     throw new ConstellationException("There is no layer in capabilities with name "+layerName);
                 }
+
+                // decode input CRS code
+                final CoordinateReferenceSystem displayCRS;
+                try {
+                    displayCRS = CRS.decode(crs);
+                } catch (FactoryException e) {
+                    throw new ConstellationException("Invalid CRS : "+crs, e);
+                }
+
+                boolean tmsFound = false;
                 final List<TileMatrixSetLink> tmslList = layerType.getTileMatrixSetLink();
-                if(tmslList!=null && !tmslList.isEmpty()){
-                    final TileMatrixSetLink tmsl = tmslList.get(0); //the layer contains only one tilematrixSetLink
-                    if(tmsl != null){
-                        final String matrixSetId = tmsl.getTileMatrixSet();
-                        map.put("matrixSet",matrixSetId);
+                if (tmslList != null && !tmslList.isEmpty()) {
 
-                        final TileMatrixSet matrixSet = contents.getTileMatrixSetByIdentifier(matrixSetId);
-                        if(matrixSet!=null){
-                            final List<TileMatrix> tileMatrixList = matrixSet.getTileMatrix();
-                            if(tileMatrixList != null){
-                                final List<String> matrixIds = new ArrayList<>();
-                                final List<Double> resolutions = new ArrayList<>();
-                                for(int i=tileMatrixList.size()-1;i>=0;i--){
-                                    final TileMatrix tm = tileMatrixList.get(i);
-                                    matrixIds.add(tm.getIdentifier().getValue());
+                    //search matching TileMatrixSet with displayCRS
+                    for (TileMatrixSetLink tmsl : tmslList) {
+                        if (tmsl != null) {
+                            final String matrixSetId = tmsl.getTileMatrixSet();
+                            map.put("matrixSet", matrixSetId);
 
-                                    try{
-                                        final CoordinateReferenceSystem crsObj = CRS.decode(matrixSet.getSupportedCRS());
-                                        final double scale = WMTSUtilities.unitsByPixel(matrixSet, crsObj, tm);
-                                        resolutions.add(scale);
-                                    }catch(Exception ex){
-                                        LOGGER.log(Level.WARNING,ex.getLocalizedMessage(),ex);
+                            final TileMatrixSet matrixSet = contents.getTileMatrixSetByIdentifier(matrixSetId);
+                            if (matrixSet != null) {
+
+                                final String supportedCRS = matrixSet.getSupportedCRS();
+                                try {
+                                    final CoordinateReferenceSystem tmsCRS = CRS.decode(supportedCRS);
+                                    if (!CRS.equalsIgnoreMetadata(tmsCRS, displayCRS)) {
+                                        continue;
                                     }
+
+                                    final List<TileMatrix> tileMatrixList = matrixSet.getTileMatrix();
+                                    if (tileMatrixList != null) {
+                                        final List<String> matrixIds = new ArrayList<>();
+                                        final List<Double> resolutions = new ArrayList<>();
+                                        for (int i = tileMatrixList.size() - 1; i >= 0; i--) {
+                                            final TileMatrix tm = tileMatrixList.get(i);
+                                            matrixIds.add(tm.getIdentifier().getValue());
+
+                                            final double scale = WMTSUtilities.unitsByPixel(matrixSet, tmsCRS, tm);
+                                            resolutions.add(scale);
+                                        }
+                                        map.put("matrixIds", matrixIds.toArray());
+                                        map.put("resolutions", resolutions.toArray());
+                                        tmsFound = true;
+                                        break;
+                                    }
+                                } catch (FactoryException e) {
+                                    LOGGER.log(Level.WARNING, e.getLocalizedMessage(), e);
                                 }
-                                map.put("matrixIds",matrixIds.toArray());
-                                map.put("resolutions",resolutions.toArray());
                             }
                         }
                     }
                 }
+
+                if (!tmsFound) {
+                    throw new ConfigurationException("No TileMatrixSet found for layer "+layerName+" and crs "+crs);
+                }
+
                 final List<Style> styleList = layerType.getStyle();
                 if(styleList != null && !styleList.isEmpty()){
                     final Style style = styleList.get(0);
                     map.put("style",style.getIdentifier().getValue());
                 }
-                final List<WGS84BoundingBoxType> bboxList = layerType.getWGS84BoundingBox();
-                final List<BoundingBoxType> bboxList2 = layerType.getBoundingBox();
-                if (bboxList2 != null && ! bboxList2.isEmpty()) {
-                    final BoundingBoxType bbt = bboxList2.get(0);
-                    map.put("dataExtent", Arrays.asList(bbt.getLowerCorner().get(0),
-                            bbt.getLowerCorner().get(1),
-                            bbt.getUpperCorner().get(0),
-                            bbt.getUpperCorner().get(1)).toArray());
-                } else if (bboxList != null && ! bboxList.isEmpty()) {
-                    final WGS84BoundingBoxType bbt = bboxList.get(0);
-                    //in case of Constellation wmts getBoundingBox() is never null.
-                    //so this case is never called, but to keep the code generic
-                    //we need to check for getWGS84BoundingBox and transform coordinates.
-                    //@TODO transform coordinates to given crs.
-                    map.put("dataExtent", Arrays.asList(bbt.getLowerCorner().get(0),
-                            bbt.getLowerCorner().get(1),
-                            bbt.getUpperCorner().get(0),
-                            bbt.getUpperCorner().get(1)).toArray());
-                }
+
+                // try to extract "dataExtent"
+                extractExtent(crs, displayCRS, layerType, map);
             }
         }
         return Response.ok(map).build();
+    }
+
+    /**
+     * Analyse LayerType BoundingBoxType.
+     *
+     * @param crs
+     * @param displayCRS
+     * @param layerType
+     * @param map
+     */
+    private void extractExtent(String crs, CoordinateReferenceSystem displayCRS, LayerType layerType, Map<String, Object> map) {
+    /*
+     * BBOX extraction logic :
+     * 1 - try to find a BBOX matching requested CRS in layer
+     * 2 - transform this BBOX in requested CRS with longitude first forced
+     * 3 - if transformed BBOX contain NaN or Infinity try to clip with CRS domain of validity
+     * 4 - if clip contain NaN or Infinity return CRS domain of validity
+     * TODO step 3 and 4 not safe
+     */
+
+        final List<WGS84BoundingBoxType> bboxList = layerType.getWGS84BoundingBox();
+        final List<BoundingBoxType> bboxList2 = layerType.getBoundingBox();
+
+        BoundingBoxType bbt = null;
+        if (bboxList2 != null && !bboxList2.isEmpty()) {
+            for (BoundingBoxType boxType : bboxList2) {
+                if (boxType.getCrs().equals(crs)) {
+                    bbt = boxType;
+                    break;
+                }
+            }
+        } else if (bboxList != null && !bboxList.isEmpty()) {
+            //use WGS84 bbox
+            bbt = bboxList.get(0);
+        }
+
+        // BoundingBoxType should not be null if we found a TileMatrixSetLink matching display CRS
+        if (bbt != null) {
+            final String bboxCRSCode = bbt.getCrs();
+            final CoordinateReferenceSystem bboxCRS;
+            try {
+                bboxCRS = CRS.decode(bboxCRSCode);
+            } catch (FactoryException e) {
+                throw new ConstellationException("Invalid bbox CRS code : "+bboxCRSCode, e);
+            }
+
+            GeneralEnvelope bboxEnv = new GeneralEnvelope(bboxCRS);
+            bboxEnv.setRange(0, bbt.getLowerCorner().get(0), bbt.getUpperCorner().get(0));
+            bboxEnv.setRange(1, bbt.getLowerCorner().get(1), bbt.getUpperCorner().get(1));
+
+            try {
+                //force output CRS with longitude first
+                AbstractCRS displayCRSLongFirst = AbstractCRS.castOrCopy(displayCRS);
+                displayCRSLongFirst = displayCRSLongFirst.forConvention(AxesConvention.NORMALIZED);
+
+                // transform bbox in displayCRSLongFirst
+                GeneralEnvelope transformedEnv = GeneralEnvelope.castOrCopy(Envelopes.transform(bboxEnv, displayCRSLongFirst));
+
+                if (isValid(transformedEnv)) {
+                    putExtent(map, transformedEnv);
+                } else {
+
+                    //transformed envelope not valid
+                    //try to clip with crs validity envelope
+                    Envelope displayEnv = CRS.getEnvelope(displayCRSLongFirst);
+                    if (displayEnv != null) {
+                        GeneralEnvelope displayValididty = GeneralEnvelope.castOrCopy(displayEnv);
+                        displayValididty.intersect(transformedEnv);
+
+                        if (isValid(displayValididty)) {
+                            //clip valid
+                            putExtent(map, displayValididty);
+                        } else {
+                            //return crs validity envelope
+                            putExtent(map, GeneralEnvelope.castOrCopy(displayEnv));
+                        }
+                    } else {
+                        throw new ConstellationException("Unable to transform bbox from "+bboxCRSCode+" to "+crs);
+                    }
+                }
+
+            } catch (TransformException e) {
+                throw new ConstellationException("Can't transform bbox from "+bboxCRSCode+" to "+crs, e);
+            }
+        }
+    }
+
+    /**
+     * Put an envelope in a map with key "dataExtent".
+     */
+    private void putExtent(Map<String, Object> map, GeneralEnvelope envelope) {
+        double[] bbox2D = new double[4];
+        bbox2D[0] = envelope.getMinimum(0);
+        bbox2D[1] = envelope.getMinimum(1);
+        bbox2D[2] = envelope.getMaximum(0);
+        bbox2D[3] = envelope.getMaximum(1);
+        map.put("dataExtent", bbox2D);
+    }
+
+    /**
+     * Test if an envelope contain NaN or Infinite values.
+     *
+     * @param envelope
+     * @return
+     */
+    private boolean isValid(GeneralEnvelope envelope) {
+
+        for (int d = 0, dim = envelope.getDimension(); d < dim; d++) {
+            if (Double.isNaN(envelope.getMinimum(d)) || Double.isNaN(envelope.getMaximum(d)) ||
+                    Double.isInfinite(envelope.getMinimum(d)) || Double.isInfinite(envelope.getMaximum(d))) return false;
+        }
+        return true;
     }
 
 }
