3e2ca8d3bb8e1652ff7c7ab4b239df7edb5da986
Guilhem Legal <guilhem.legal@geomatys.com>
2015-08-25 17:18:25 +0200

    fix(Library): fix a test data with a percentage superior to 100

--- modules/library/index/generic/src/test/java/org/constellation/metadata/index/generic/GenericindexTest.java
+++ modules/library/index/generic/src/test/java/org/constellation/metadata/index/generic/GenericindexTest.java
@@ -507,9 +507,8 @@ public class GenericindexTest {
 
         assertTrue(result.contains("42292_5p_19900609195600"));
 
-        //FIXME
-        //assertTrue(result.contains("39727_22_19750113062500"));
-        //assertEquals(2, result.size());
+        assertTrue(result.contains("39727_22_19750113062500"));
+        assertEquals(2, result.size());
 
         /**
          * Test 4 numeric search: CloudCover => 60
@@ -542,8 +541,7 @@ public class GenericindexTest {
         expectedResult = new LinkedHashSet<>();
         expectedResult.add("42292_5p_19900609195600");
 
-        //FIXME
-        //expectedResult.add("39727_22_19750113062500");
+        expectedResult.add("39727_22_19750113062500");
 
         //issues here it found
         assertEquals(expectedResult, result);
@@ -851,8 +849,7 @@ public class GenericindexTest {
         expectedResult.add("42292_5p_19900609195600");
         expectedResult.add("42292_9s_19900610041000");
 
-        //FIXME
-        //expectedResult.add("39727_22_19750113062500");
+        expectedResult.add("39727_22_19750113062500");
         assertEquals(expectedResult, result);
 
         /**
@@ -872,8 +869,7 @@ public class GenericindexTest {
         LOGGER.log(Level.FINER, "SortedSearch 5:\n{0}", resultReport);
 
         expectedResult = new LinkedHashSet<>();
-        //FIXME
-        //expectedResult.add("39727_22_19750113062500");
+        expectedResult.add("39727_22_19750113062500");
         expectedResult.add("42292_5p_19900609195600");
         expectedResult.add("42292_9s_19900610041000");
 
