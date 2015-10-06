9ec2706691e183c18c83a4ec2fe0090ec0626871
Quentin Boileau <quentin.boileau@gmail.com>
2015-10-06 11:12:46 +0200

    feat(Admin): add logout URL and refresh in application configuration response

--- modules/cstl-admin/src/main/java/org/constellation/admin/security/CstlAdminLoginConfigurationService.java
+++ modules/cstl-admin/src/main/java/org/constellation/admin/security/CstlAdminLoginConfigurationService.java
@@ -11,9 +11,11 @@ public class CstlAdminLoginConfigurationService {
     private static final Logger LOGGER = LoggerFactory.getLogger(CstlAdminLoginConfigurationService.class);
     
     private String cstlLoginURL = "login.html";
-    
+    private String cstlLogoutURL = null;
+    private String cstlRefreshURL = null;
+
     public CstlAdminLoginConfigurationService() {
-         LOGGER.debug("CstlAdminLoginConfigurationService construct");
+         LOGGER.debug("***** CstlAdminLoginConfigurationService construct *****");
     }
     
     
@@ -25,6 +27,30 @@ public class CstlAdminLoginConfigurationService {
         LOGGER.info("CSTL Login page changed to " + cstlLoginURL);
         this.cstlLoginURL = cstlLoginURL;
     }
-    
 
+    /**
+     * Logout URL
+     * @return Cstl logout URL, can be null
+     */
+    public String getCstlLogoutURL() {
+        return cstlLogoutURL;
+    }
+
+    public void setCstlLogoutURL(String cstlLogoutURL) {
+        LOGGER.info("CSTL Logout page changed to " + cstlLogoutURL);
+        this.cstlLogoutURL = cstlLogoutURL;
+    }
+
+    /**
+     * Refresh token URL
+     * @return refresh URL, can be null
+     */
+    public String getCstlRefreshURL() {
+        return cstlRefreshURL;
+    }
+
+    public void setCstlRefreshURL(String cstlRefreshURL) {
+        LOGGER.info("CSTL Refresh token page changed to " + cstlRefreshURL);
+        this.cstlRefreshURL = cstlRefreshURL;
+    }
 }
