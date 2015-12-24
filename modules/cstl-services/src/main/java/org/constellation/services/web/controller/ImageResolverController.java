38e5e2c87459c06babc924be88eba1c34e6c3b92
msidhoum <mehdi.sidhoum@geomatys.com>
2015-12-24 11:10:17 +0100

    fix(Services): fix content-type for image resolver used by metadata graphicOverview field (CSTL-1591).

--- modules/cstl-services/src/main/java/org/constellation/services/web/controller/ImageResolverController.java
+++ modules/cstl-services/src/main/java/org/constellation/services/web/controller/ImageResolverController.java
@@ -1,8 +1,12 @@
 package org.constellation.services.web.controller;
 
 import org.constellation.configuration.ConfigDirectory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.nio.file.Files;
 
 /**
  * Spring servlet to resolve resources images for metadata thumbnails.
@@ -23,26 +29,36 @@ import java.io.File;
 @RequestMapping("/resolveImage")
 public class ImageResolverController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageResolverController.class);
+
+
     @RequestMapping(value = "/{mdIdentifierSHA1}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity get(@PathVariable("mdIdentifierSHA1") String mdIdentifierSHA1) {
+    public ResponseEntity get(@PathVariable("mdIdentifierSHA1") String mdIdentifierSHA1,
+                              final HttpServletResponse response) {
         if(mdIdentifierSHA1 != null) {
             final File metadataCfgDir = ConfigDirectory.getMetadataDirectory();
             final File metadataFolder = new File(metadataCfgDir, mdIdentifierSHA1);
             final File quickLook = new File(metadataFolder, mdIdentifierSHA1);
             if (metadataFolder.exists()) {
-                return ok(new FileSystemResource(quickLook));
+                response.setCharacterEncoding("UTF-8");
+                final HttpHeaders responseHeaders = new HttpHeaders();
+                try {
+                    //try to get the content type of the file
+                    final String contentType = Files.probeContentType(quickLook.toPath());
+                    final String mediaType = MediaType.valueOf(contentType).toString();
+                    response.setContentType(mediaType);
+                    responseHeaders.set("Content-Type", mediaType);
+                }catch(Exception ex) {
+                    LOGGER.warn(ex.getLocalizedMessage(), ex);
+                    response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+                    responseHeaders.set("Content-Type", MediaType.APPLICATION_OCTET_STREAM_VALUE);
+                }
+                final FileSystemResource fsResource = new FileSystemResource(quickLook);
+                return new ResponseEntity<>(fsResource,responseHeaders,HttpStatus.OK);
             }
         }
         return null;
-    }
-
-    private <T> ResponseEntity<T> entity(T entity, HttpStatus httpStatus) {
-        return new ResponseEntity<T>(entity, httpStatus);
-    }
-
-    public <T> ResponseEntity<T> ok(T entity) {
-        return entity(entity, HttpStatus.OK);
     }
 
 }
